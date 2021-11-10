2defc6c3db3dcd146635f564b9a8b6ae7d6f2d9b
Alex Becker <becker.m.alex@gmail.com>
2021-11-10 18:00:06 +0100

    Issues #7 + #8: make timebudget + datatypes configurable

--- AutoMLs/StructuredDataAutoML.py
+++ AutoMLs/StructuredDataAutoML.py
@@ -3,10 +3,26 @@ import pandas as pd
 import pickle
 from flaml import AutoML
 
+from enum import Enum, unique
+
+
+@unique
+class DataType(Enum):
+    DATATYPE_UNKNOW = 0
+    DATATYPE_STRING = 1
+    DATATYPE_INT = 2
+    DATATYPE_FLOAT = 3
+    DATATYPE_CATEGORY = 4
+    DATATYPE_BOOLEAN = 5
+    DATATYPE_DATETIME = 6
+    DATATYPE_IGNORE = 7
+
+
 class StructuredDataAutoML(object):
     """
     Implementation of the AutoML functionality fo structured data a.k.a. tabular data
     """
+
     def __init__(self, configuration: dict):
         """
         Init a new instance of StructuredDataAutoML
@@ -15,6 +31,9 @@ class StructuredDataAutoML(object):
         1. Configuration JSON of type dictionary
         """
         self.__configuration = configuration
+        # set default values
+        if self.__configuration["time_budget"] == 0:
+            self.__configuration["time_budget"] = 20
 
     def __read_training_data(self):
         """
@@ -23,6 +42,11 @@ class StructuredDataAutoML(object):
         df = pd.read_csv(os.path.join(self.__configuration["file_location"], self.__configuration["file_name"]))
         self.__X = df.drop(self.__configuration["configuration"]["target"], axis=1)
         self.__y = df[self.__configuration["configuration"]["target"]]
+
+    def __feature_selection(self):
+        for column, type in self.__configuration["configuration"]["features"].items():
+            if DataType(type) is DataType.DATATYPE_IGNORE:
+                self.__X = self.__X.drop(column, axis=1)
 
     def __export_model(self, model):
         """
@@ -48,9 +72,10 @@ class StructuredDataAutoML(object):
         Execute the classification task
         """
         self.__read_training_data()
+        self.__feature_selection()
         automl = AutoML()
         automl_settings = {
-            "time_budget": 10,
+            "time_budget": self.__configuration["time_budget"],
             "metric": 'accuracy',
             "task": 'classification',
             "log_file_name": 'flaml.log',
@@ -64,9 +89,10 @@ class StructuredDataAutoML(object):
         Execute the regression task
         """
         self.__read_training_data()
+        self.__feature_selection()
         automl = AutoML()
         automl_settings = {
-            "time_budget": 10,
+            "time_budget": self.__configuration["time_budget"],
             "metric": 'rmse',
             "task": 'regression',
             "log_file_name": 'flaml.log',
