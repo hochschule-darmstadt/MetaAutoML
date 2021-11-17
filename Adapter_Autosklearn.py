65793f7fc32f6c3cc951f0334f50a095184a535e
Jonas Weßner <jonas.wessner@gmx.de>
2021-11-17 08:04:09 +0100

    #1 refactor Adapter_autosklearn.py: functions instead of methods, because they could be static; remove unnecessary assignment in init

--- Adapter_Autosklearn.py
+++ Adapter_Autosklearn.py
@@ -15,11 +15,81 @@ from TemplateGenerator import TemplateGenerator
 from OsSpecific import in_cluster, on_unix
 
 
+def get_except_response(context, e):
+    print(e)
+    context.set_details(f"Error while executing AutoKeras: {e}")
+    context.set_code(grpc.StatusCode.UNAVAILABLE)
+    return Adapter_pb2.StartAutoMLResponse()
+
+
+def generate_script():
+    generator = TemplateGenerator()
+    generator.GenerateScript()
+
+
+def capture_process_output(process):
+    capture = ""
+    s = process.stdout.read(1)
+    capture += s
+    # Run until no more output is produced by the subprocess
+    while len(s) > 0:
+        if capture[len(capture) - 1] == '\n':
+            processUpdate = Adapter_pb2.StartAutoMLResponse()
+            processUpdate.returnCode = Adapter_pb2.ADAPTER_RETURN_CODE_STATUS_UPDATE
+            processUpdate.statusUpdate = capture
+            processUpdate.outputJson = ""
+            yield processUpdate
+            sys.stdout.write(capture)
+            sys.stdout.flush()
+            capture = ""
+        capture += s
+        s = process.stdout.read(1)
+
+
+def get_response(output_json):
+    response = Adapter_pb2.StartAutoMLResponse()
+    response.returnCode = Adapter_pb2.ADAPTER_RETURN_CODE_SUCCESS
+    response.outputJson = json.dumps(output_json)
+    yield response
+
+
+def zip_script():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    export_zip_file_name = get_config_property("export-zip-file-name")
+    if in_cluster():
+        print("RUNNING DOCKER")
+        if not os.path.exists("omaml/output"):  # ensure output folder exists
+            os.makedirs("omaml/output")
+        zip_content_path = os.path.join(BASE_DIR, "templates/output")
+        shutil.make_archive(export_zip_file_name, 'zip', zip_content_path)
+        shutil.move(f"{export_zip_file_name}.zip", "omaml/output/keras-export.zip")
+        outputJson = {"file_name": f"{export_zip_file_name}.zip"}
+        outputJson.update({"file_location": "omaml/output/"})
+
+    else:
+        print("RUNNING LOCAL")
+        zip_content_path = os.path.join(BASE_DIR, get_config_property("output-path"))
+        shutil.make_archive(export_zip_file_name, 'zip', zip_content_path)
+        outputJson = {"file_name": f"templates/output/{export_zip_file_name}.zip"}
+        outputJson.update({"file_location": os.path.join(BASE_DIR, "Adapter-AutoKeras")})
+    return outputJson
+
+
+def start_automl_process():
+    """"
+    starts the automl process with respect to the operating system
+    @:return started automl process
+    """
+    return subprocess.Popen([get_config_property("python-environment"), "AutoML.py", ""],
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True)
+
+
 class AdapterServiceServicer(Adapter_pb2_grpc.AdapterServiceServicer):
     """ AutoML Adapter Service implementation. Service provide functionality to execute and interact with the current AutoML process. """
 
     def __init__(self):
-        self = self
+        pass
 
     def StartAutoML(self, request, context):
         """ 
@@ -30,79 +100,15 @@ class AdapterServiceServicer(Adapter_pb2_grpc.AdapterServiceServicer):
             with open(get_config_property("job-file-name"), "w+") as f:
                 json.dump(request.processJson, f)
 
-            process = self.start_automl_process()
-            yield from self.capture_process_output(process)
-            self.generate_script()
-            outputJson = self.zip_script()
+            process = start_automl_process()
+            yield from capture_process_output(process)
+            generate_script()
+            outputJson = zip_script()
 
-            yield from self.get_response(outputJson)
+            yield from get_response(outputJson)
 
         except Exception as e:
-            return self.get_except_response(context, e)
-
-    def get_except_response(self, context, e):
-        print(e)
-        context.set_details(f"Error while executing AutoKeras: {e}")
-        context.set_code(grpc.StatusCode.UNAVAILABLE)
-        return Adapter_pb2.StartAutoMLResponse()
-
-    def generate_script(self):
-        generator = TemplateGenerator()
-        generator.GenerateScript()
-
-    def capture_process_output(self, process):
-        capture = ""
-        s = process.stdout.read(1)
-        capture += s
-        # Run until no more output is produced by the subprocess
-        while len(s) > 0:
-            if capture[len(capture) - 1] == '\n':
-                processUpdate = Adapter_pb2.StartAutoMLResponse()
-                processUpdate.returnCode = Adapter_pb2.ADAPTER_RETURN_CODE_STATUS_UPDATE
-                processUpdate.statusUpdate = capture
-                processUpdate.outputJson = ""
-                yield processUpdate
-                sys.stdout.write(capture)
-                sys.stdout.flush()
-                capture = ""
-            capture += s
-            s = process.stdout.read(1)
-
-    def get_response(self, outputJson):
-        response = Adapter_pb2.StartAutoMLResponse()
-        response.returnCode = Adapter_pb2.ADAPTER_RETURN_CODE_SUCCESS
-        response.outputJson = json.dumps(outputJson)
-        yield response
-
-    def zip_script(self):
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        export_zip_file_name = get_config_property("export-zip-file-name")
-        if in_cluster():
-            print("RUNNING DOCKER")
-            if not os.path.exists("omaml/output"):  # ensure output folder exists
-                os.makedirs("omaml/output")
-            zip_content_path = os.path.join(BASE_DIR, "templates/output")
-            shutil.make_archive(export_zip_file_name, 'zip', zip_content_path)
-            shutil.move(f"{export_zip_file_name}.zip", "omaml/output/keras-export.zip")
-            outputJson = {"file_name": f"{export_zip_file_name}.zip"}
-            outputJson.update({"file_location": "omaml/output/"})
-
-        else:
-            print("RUNNING LOCAL")
-            zip_content_path = os.path.join(BASE_DIR, get_config_property("output-path"))
-            shutil.make_archive(export_zip_file_name, 'zip', zip_content_path)
-            outputJson = {"file_name": f"templates/output/{export_zip_file_name}.zip"}
-            outputJson.update({"file_location": os.path.join(BASE_DIR, "Adapter-AutoKeras")})
-        return outputJson
-
-    def start_automl_process(self):
-        """"
-        starts the automl process with respect to the operating system
-        @:return started automl process
-        """
-        return subprocess.Popen([get_config_property("python-environment"), "AutoML.py", ""],
-                                stdout=subprocess.PIPE,
-                                universal_newlines=True)
+            return get_except_response(context, e)
 
 
 def serve():
