34f893a856a8960f1ea94923008bbe46a9769262
Alex Becker <becker.m.alex@gmail.com>
2021-11-18 13:21:26 +0100

    make flaml-job.json to actual json
    (and not str)
    + reformating

--- AutoML.py
+++ AutoML.py
@@ -4,14 +4,14 @@ import logging
 import sys
 
 from AutoMLs.StructuredDataAutoML import StructuredDataAutoML
+from JsonUtil import get_config_property
 
 if __name__ == '__main__':
     FORMAT = '%(asctime)s %(message)s'
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=FORMAT)
 
-    json_file = open('flaml-job.json')
+    json_file = open(get_config_property("job-file-name"))
     processJson = json.load(json_file)
-    processJson = json.loads(processJson)
 
     structuredDataAutoML = StructuredDataAutoML(processJson)
     structuredDataAutoML.execute_task()
