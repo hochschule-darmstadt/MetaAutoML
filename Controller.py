2683f513d405ba0b34a02064f6452a949ecf5718
Jonas Weßner <jonas.wessner@gmx.de>
2021-11-21 16:37:25 +0100

    #1 refactor: remove if statement, because the content of each branch was identical

--- Controller.py
+++ Controller.py
@@ -16,6 +16,7 @@ import json
 
 from ControllerManager import ControllerManager
 
+
 def _load_credential_from_file(filepath):
     """
     Load the certificate from disk
@@ -29,26 +30,23 @@ def _load_credential_from_file(filepath):
     with open(real_path, 'rb') as f:
         return f.read()
 
+
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 SERVER_CERTIFICATE = _load_credential_from_file('certificate/server.crt')
 SERVER_CERTIFICATE_KEY = _load_credential_from_file('certificate/server.key')
-#ROOT_CERTIFICATE = _load_credential_from_file('certificate/root.crt')
+
+
+# ROOT_CERTIFICATE = _load_credential_from_file('certificate/root.crt')
 
 
 class ControllerServiceServicer(Controller_pb2_grpc.ControllerServiceServicer):
     """includes all gRPC functions available for the client frontend"""
-    
+
     def __init__(self):
-        self = self
-        try:
-          if os.environ["RUNTIME"]: #Only available in Cluster
-            DATASET_FOLDER = os.path.join(ROOT_PATH, 'omaml/datasets')
-            self._controllerManager = ControllerManager(DATASET_FOLDER)
-        except KeyError: # Raise error if the variable is not set, only for local run
-            DATASET_FOLDER = os.path.join(ROOT_PATH, 'omaml/datasets')
-            self._controllerManager = ControllerManager(DATASET_FOLDER)
-            
+        DATASET_FOLDER = os.path.join(ROOT_PATH, 'omaml/datasets')
+        self._controllerManager = ControllerManager(DATASET_FOLDER)
+
     def GetAutoMlModel(self, request, context):
         """ return the generated model as a .zip for one AutoML by its session id."""
         response = self._controllerManager.GetAutoMlModel(request)
@@ -59,7 +57,7 @@ class ControllerServiceServicer(Controller_pb2_grpc.ControllerServiceServicer):
         """
         response = self._controllerManager.GetCompatibleAUtoMlSolutions(request)
         return response
-            
+
     def GetDatasets(self, request, context):
         """return all datasets of a specific type."""
         datasets = self._controllerManager.GetDatasets()
@@ -106,18 +104,19 @@ class ControllerServiceServicer(Controller_pb2_grpc.ControllerServiceServicer):
         response = self._controllerManager.StartAutoMLProcess(request)
         return response
 
+
 def serve():
     """
     Boot the gRPC server and wait for incoming connections
     """
     server_credentials = grpc.ssl_server_credentials(((
-        SERVER_CERTIFICATE_KEY,
-        SERVER_CERTIFICATE,
-    ),))
-    
+                                                          SERVER_CERTIFICATE_KEY,
+                                                          SERVER_CERTIFICATE,
+                                                      ),))
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     Controller_pb2_grpc.add_ControllerServiceServicer_to_server(ControllerServiceServicer(), server)
-    #server.add_insecure_port('[::]:50051')
+    # server.add_insecure_port('[::]:50051')
     server.add_secure_port('0.0.0.0:5001', server_credentials)
 
     server.start()
@@ -128,4 +127,3 @@ if __name__ == '__main__':
     logging.basicConfig()
     serve()
     print('done.')
-    
