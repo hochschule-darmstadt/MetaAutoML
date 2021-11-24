ed26115a19c4fb66d893cb185b6f04187c5ad7d5
David Reyer <david.reyer@stud.h-da.de>
2021-11-24 07:20:08 -0500

    rebasing onto current master cause im too dumb to pull before committing

---
+++ managers/structureddata/AutoPyTorchManager.py
@@ -0,0 +1,12 @@
+from AutoMLManager import AutoMLManager
+
+
+class AutoPyTorchManager(AutoMLManager):
+    name = "AutoPyTorch"
+
+    def __init__(self, configuration, folder_location):
+        automl_service_host = 'AUTOPYTORCH_SERVICE_HOST'
+        automl_service_port = 'AUTOPYTORCH_SERVICE_PORT'
+        automl_default_port = '50059'
+        super(AutoPyTorchManager, self).__init__(configuration, folder_location, automl_service_host, automl_service_port,
+                                             automl_default_port)
