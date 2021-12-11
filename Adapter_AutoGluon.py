7d23867d26d9467faadb54de9cbe8782650778ae
Jonas Weßner <jonas.wessner@gmx.de>
2021-12-11 14:30:56 +0100

    #9 remove deprecated import

--- Adapter_AutoGluon.py
+++ Adapter_AutoGluon.py
@@ -12,7 +12,6 @@ import Adapter_pb2_grpc
 
 from concurrent import futures
 from TemplateGenerator import TemplateGenerator
-from OsSpecific import in_cluster
 
 
 def get_except_response(context, e):
