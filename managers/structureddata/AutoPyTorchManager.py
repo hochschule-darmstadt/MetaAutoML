6131793bdd8d3c276aee6b92e94dfebdeb1418bc
Jonas Weßner <jonas.wessner@gmx.de>
2021-12-14 11:59:01 +0100

    rename pytorch environment varialbe

--- managers/structureddata/AutoPyTorchManager.py
+++ managers/structureddata/AutoPyTorchManager.py
@@ -5,7 +5,7 @@ class AutoPyTorchManager(AutoMLManager):
     name = "Auto_PyTorch"
 
     def __init__(self, configuration, folder_location):
-        automl_service_host = 'AUTOPYTORCH_SERVICE_HOST'
-        automl_service_port = 'AUTOPYTORCH_SERVICE_PORT'
+        automl_service_host = 'PYTORCH_SERVICE_HOST'
+        automl_service_port = 'PYTORCH_SERVICE_PORT'
         super(AutoPyTorchManager, self).__init__(configuration, folder_location, automl_service_host,
                                                  automl_service_port)
