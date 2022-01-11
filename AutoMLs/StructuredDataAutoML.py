b35b550fbde14c62fd8726311195f77164e7dd1b
Dave <davidtreyer1@googlemail.com>
2022-01-11 17:26:16 +0100

    [#22](https://github.com/hochschule-darmstadt/MetaAutoML/issues/22) implements a new parameter "metric" in the grpc files and the adapters. This metric parameter is used to set the metric the adapter should optimize for. If the field is kept empty each adapter uses its default value.

--- AutoMLs/StructuredDataAutoML.py
+++ AutoMLs/StructuredDataAutoML.py
@@ -34,6 +34,10 @@ class StructuredDataAutoML(object):
         if self.__configuration["runtime_constraints"]["max_iter"] == 0:
             self.__configuration["runtime_constraints"]["max_iter"] = 3
 
+        if self.__configuration["metric"] == "":
+            # handle empty metric field, None is the default metric parameter for AutoKeras
+            self.__configuration["metric"] = None
+
     def __read_training_data(self):
         """
         Read the training dataset from disk
@@ -86,6 +90,7 @@ class StructuredDataAutoML(object):
         self.__dataset_preparation()
         clf = ak.StructuredDataClassifier(overwrite=True,
                                           max_trials=self.__configuration["runtime_constraints"]["max_iter"],
+                                          metric=self.__configuration['metric'],
                                           seed=42)
         clf.fit(x=self.__X, y=self.__y)
         self.__export_model(clf)
@@ -96,6 +101,7 @@ class StructuredDataAutoML(object):
         self.__dataset_preparation()
         reg = ak.StructuredDataRegressor(overwrite=True,
                                          max_trials=self.__configuration["runtime_constraints"]["max_iter"],
+                                         metric=self.__configuration['metric'],
                                          seed=42)
         reg.fit(x=self.__X, y=self.__y)
         self.__export_model(reg)
