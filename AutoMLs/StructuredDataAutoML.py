78d3276ba5643d8c8bc176c05b0c780d92efc565
Dave <davidtreyer1@googlemail.com>
2022-01-11 17:26:15 +0100

    [#22](https://github.com/hochschule-darmstadt/MetaAutoML/issues/22) implements a new parameter "metric" in the grpc files and the adapters. This metric parameter is used to set the metric the adapter should optimize for. If the field is kept empty each adapter uses its default value.

--- AutoMLs/StructuredDataAutoML.py
+++ AutoMLs/StructuredDataAutoML.py
@@ -34,6 +34,11 @@ class StructuredDataAutoML(object):
         """
         self.__time_limit = 30
         self.__configuration = configuration
+
+        if self.__configuration["metric"] == "":
+            # handle empty metric field, None is the default metric parameter for AutoSklearn
+            self.__configuration["metric"] = None
+
         return
 
     def __read_training_data(self):
@@ -95,6 +100,7 @@ class StructuredDataAutoML(object):
         if self.__configuration["runtime_constraints"]["runtime_limit"] != 0:
             automl_settings.update(
                 {"time_left_for_this_task": self.__configuration["runtime_constraints"]["runtime_limit"]})
+        automl_settings.update({"metric": self.__configuration["metric"]})
         return automl_settings
 
     def __classification(self):
