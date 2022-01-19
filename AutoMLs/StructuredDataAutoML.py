e1ba04df75edc3c266fb6706d6c6959c71b38377
Alex Becker <becker.m.alex@gmail.com>
2022-01-19 09:56:33 +0100

    #35 calculate test score

--- AutoMLs/StructuredDataAutoML.py
+++ AutoMLs/StructuredDataAutoML.py
@@ -1,21 +1,11 @@
 import os
+import dill
 import pandas as pd
 import autokeras as ak
 
 from enum import Enum, unique
 from JsonUtil import get_config_property
-
-
-@unique
-class DataType(Enum):
-    DATATYPE_UNKNOW = 0
-    DATATYPE_STRING = 1
-    DATATYPE_INT = 2
-    DATATYPE_FLOAT = 3
-    DATATYPE_CATEGORY = 4
-    DATATYPE_BOOLEAN = 5
-    DATATYPE_DATETIME = 6
-    DATATYPE_IGNORE = 7
+from predict_time_sources import feature_preparation, DataType, SplitMethod
 
 
 class StructuredDataAutoML(object):
@@ -37,35 +27,29 @@ class StructuredDataAutoML(object):
     def __read_training_data(self):
         """
         Read the training dataset from disk
-        In case of AutoKeras only provide the training file path
         """
         df = pd.read_csv(os.path.join(self.__configuration["file_location"], self.__configuration["file_name"]),
                          **self.__configuration["file_configuration"])
+
+        # split training set
+        if SplitMethod.SPLIT_METHOD_RANDOM == self.__configuration["test_configuration"]["method"]:
+            df = df.sample(random_state=self.__configuration["test_configuration"]["random_state"], frac=1)
+        else:
+            df = df.iloc[:int(df.shape[0] * self.__configuration["test_configuration"]["split_ratio"])]
+
         target = self.__configuration["tabular_configuration"]["target"]["target"]
         self.__X = df.drop(target, axis=1)
         self.__y = df[target]
 
     def __dataset_preparation(self):
-        for column, dt in self.__configuration["tabular_configuration"]["features"].items():
-            if DataType(dt) is DataType.DATATYPE_IGNORE:
-                self.__X = self.__X.drop(column, axis=1)
-            elif DataType(dt) is DataType.DATATYPE_CATEGORY:
-                self.__X[column] = self.__X[column].astype('category')
-            elif DataType(dt) is DataType.DATATYPE_BOOLEAN:
-                self.__X[column] = self.__X[column].astype('bool')
-            elif DataType(dt) is DataType.DATATYPE_INT:
-                self.__X[column] = self.__X[column].astype('int')
-            elif DataType(dt) is DataType.DATATYPE_FLOAT:
-                self.__X[column] = self.__X[column].astype('float')
+        feature_preparation(self.__X, self.__configuration["tabular_configuration"]["features"].items())
         self.__cast_target()
 
     def __cast_target(self):
         target_dt = self.__configuration["tabular_configuration"]["target"]["type"]
-        if DataType(target_dt) is DataType.DATATYPE_CATEGORY:
-            self.__y = self.__y.astype('category')
-        elif DataType(target_dt) is DataType.DATATYPE_BOOLEAN:
-            self.__y = self.__y.astype('bool')
-        elif DataType(target_dt) is DataType.DATATYPE_INT:
+        if DataType(target_dt) is DataType.DATATYPE_CATEGORY or \
+                DataType(target_dt) is DataType.DATATYPE_BOOLEAN or \
+                DataType(target_dt) is DataType.DATATYPE_INT:
             self.__y = self.__y.astype('int')
         elif DataType(target_dt) is DataType.DATATYPE_FLOAT:
             self.__y = self.__y.astype('float')
@@ -77,10 +61,8 @@ class StructuredDataAutoML(object):
         Parameter:
         1. generate ML model
         """
-        model = model.export_model()
-        model.summary()
-        output_file = os.path.join(get_config_property('output-path'), "model_autokeras.p")
-        model.save(output_file, save_format="tf")
+        with open(os.path.join(get_config_property('output-path'), 'tmp', 'model_keras.p'), 'wb') as file:
+            dill.dump(model, file)
 
     def execute_task(self):
         """Execute the ML task"""
