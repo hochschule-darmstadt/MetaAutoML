4e5c0f704af1fa56987c582d2f7b4ba6342352cb
Rene <rene.binder@stud.h-da.de>
2022-05-29 15:47:57 +0200

    tidying up and basic outlier detection

--- controller/managers/DataSetAnalysisManager.py
+++ controller/managers/DataSetAnalysisManager.py
@@ -2,7 +2,6 @@ from typing import Dict
 import pandas as pd
 import numpy as np
 import json
-from collections import defaultdict
 
 class DataSetAnalysisManager:
     """
@@ -18,17 +17,19 @@ class DataSetAnalysisManager:
         ---
         Return None
         """
-        jsonfile = defaultdict(dict)
+        jsonfile = {}
+        jsonfile["basic analysis"] = {}
         jsonfile = DataSetAnalysisManager.__number_of_rows_and_columns(dataset, jsonfile)
         jsonfile = DataSetAnalysisManager.__missing_values(dataset, jsonfile)
-        #jsonfile = DataSetAnalysisManager.__detect_outliers(dataset, jsonfile)
-        print(json.dumps(jsonfile))
+        jsonfile = DataSetAnalysisManager.__detect_outliers(dataset, jsonfile)
+
+        # print(json.dumps(jsonfile))
 
         with open('controller\app-data\analysis.json', 'w+') as target:
             json.dump(jsonfile, target)
         
     @staticmethod
-    def __number_of_rows_and_columns(dataset: pd.DataFrame, jsonfile: defaultdict) -> defaultdict:
+    def __number_of_rows_and_columns(dataset: pd.DataFrame, jsonfile: dict) -> dict:
         """
         Counts the number of rows and columns of the dataset and adds that information to a JSON file
         ---
@@ -46,7 +47,7 @@ class DataSetAnalysisManager:
         return jsonfile
 
     @staticmethod
-    def __missing_values(dataset: pd.DataFrame, jsonfile: defaultdict) -> defaultdict:
+    def __missing_values(dataset: pd.DataFrame, jsonfile: dict) -> dict:
         """
         Counts missing values of each column and row and adds that information to a JSON file
         ---
@@ -56,26 +57,71 @@ class DataSetAnalysisManager:
         ---
         Return the JSON file with additional data
         """
+
+        jsonfile["basic analysis"]["missing values in columns"] = {}
+        jsonfile["basic analysis"]["rows with many missing values"] = {}
+
         dataset_missings_columns = dataset.isna().sum()
         for index, value in dataset_missings_columns.items():
 
-            jsonfile["missing values in columns"][index] = value
+            jsonfile["basic analysis"]["missing values in columns"][index] = value
 
         dataset_missings_rows = dataset.isna().sum(axis=1)
     
         for index, value in dataset_missings_rows[dataset_missings_rows > len(dataset.columns)*0.5].items():
 
-            jsonfile["rows with many missing values"][index] = value
+            jsonfile["basic analysis"]["rows with many missing values"][index] = value
 
         
         return jsonfile
 
     @staticmethod
-    def __detect_outliers(dataset: pd.DataFrame, json: defaultdict) -> defaultdict:
+    def __detect_outliers(dataset: pd.DataFrame, jsonfile: dict) -> dict:
         """
-        TODO
+        Detects outliers in all columns of the dataset containing floats and stores their indices in a JSON file
+        ---
+        Parameter
+        1. dataset to be analyzed
+        2. JSON file to write to
+        ---
+        Return the JSON file with additional data
         """
-        pass
+
+        jsonfile["basic analysis"]["outlier"] = {}
+
+        dataset_numerics_only = dataset.select_dtypes(include=[np.float])   
+    
+        for column in dataset_numerics_only:
+            
+            current_column = dataset_numerics_only[column].dropna()
+
+            current_column_sorted = current_column.sort_values()
+
+            Q1, Q2, Q3 = np.percentile(current_column_sorted, [25,50,75])
+
+            if ((Q3 - Q1) < 0.1) :
+                continue
+            
+            # interquartile range
+            IQR = Q3 - Q1
+
+            # calculation of boundaries is still a work in progress
+            lower_boundary = Q1 - 2 * IQR
+            upper_boundary = Q3 + 2 * IQR
+
+            current_column = (current_column < lower_boundary) | (current_column > upper_boundary)
+
+            outlier_indices = current_column[current_column].index.values.tolist()
+
+            outlier_indices = [index + 1 for index in outlier_indices]
+
+            jsonfile["basic analysis"]["outlier"][column] = outlier_indices
+        
+        return jsonfile
+
+
+
+
 
 
     
