c7a105b60ec8bd3b69a83a52e188fb2d4f3f9185
Rene <rene.binder@stud.h-da.de>
2022-06-04 14:16:15 +0200

    refactoring due to change request

--- controller/managers/DataSetAnalysisManager.py
+++ controller/managers/DataSetAnalysisManager.py
@@ -18,15 +18,21 @@ class DataSetAnalysisManager:
         Return a python dictionary containing dataset analysis
         """
         jsonfile = {}
-        jsonfile["basic analysis"] = {}
-        jsonfile = DataSetAnalysisManager.__number_of_rows_and_columns(dataset, jsonfile)
-        jsonfile = DataSetAnalysisManager.__missing_values(dataset, jsonfile)
-        jsonfile = DataSetAnalysisManager.__detect_outliers(dataset, jsonfile)
-        #jsonfile = DataSetAnalysisManager.__detect_duplicate_columns(dataset, jsonfile)
-        #jsonfile = DataSetAnalysisManager.__detect_duplicate_rows(dataset, jsonfile)
+        jsonfile["basic analysis"] = {
+
+            "number_of_columns": DataSetAnalysisManager.__number_of_columns(dataset),
+            "number_of_rows": DataSetAnalysisManager.__number_of_rows(dataset),
+            "na_columns": DataSetAnalysisManager.__missing_values_columns(dataset),
+            "high_na_rows": DataSetAnalysisManager.__missing_values_rows(dataset),
+            "outlier": DataSetAnalysisManager.__detect_outliers(dataset),
+            #"duplicate_columns": DataSetAnalysisManager.__detect_duplicate_columns(dataset),
+            #"duplicate_rows": DataSetAnalysisManager.__detect_duplicate_rows(dataset),
+        }
 
         # for debugging purposes
-        # print(json.dumps(jsonfile))
+        print(json.dumps(jsonfile))
+
+        return jsonfile
 
     def persistAnalysisResult(jsonfile:dict):
         """
@@ -41,97 +47,110 @@ class DataSetAnalysisManager:
             json.dump(jsonfile, target)
             target.close()
 
-        
     @staticmethod
-    def __number_of_rows_and_columns(dataset: pd.DataFrame, jsonfile: dict) -> dict:
+    def __number_of_columns(dataset: pd.DataFrame) -> dict:
         """
-        Counts the number of rows and columns of the dataset and adds that information to a JSON file
+        Counts the number of columns in the dataset and adds that information to a JSON file
         ---
         Parameter
         1. dataset to be analyzed
-        2. JSON file to write to
         ---
-        Return the JSON file with additional data
+        Return the number of columns in the dataset
         """
-        rows, columns = dataset.shape
-
-        jsonfile["basic analysis"]["number of rows"] = rows
-        jsonfile["basic analysis"]["number of columns"] = columns
-
-        return jsonfile
+        number_of_columns = dataset.shape[1]
+        
+        return number_of_columns
 
     @staticmethod
-    def __missing_values(dataset: pd.DataFrame, jsonfile: dict) -> dict:
+    def __number_of_rows(dataset: pd.DataFrame) -> dict:
+        """
+        Counts the number of rows in the dataset and adds that information to a JSON file
+        ---
+        Parameter
+        1. dataset to be analyzed
+        ---
+        Return the number of rows in the dataset
+        """
+        number_of_rows = dataset.shape[0]
+
+        return number_of_rows
+
+    @staticmethod
+    def __missing_values_columns(dataset: pd.DataFrame) -> dict:
         """
         Counts missing values of each column and row and adds that information to a JSON file
         ---
         Parameter
         1. dataset to be analyzed
-        2. JSON file to write to
         ---
-        Return the JSON file with additional data
+        Return a dictionary containing information about the missing values in columns
         """
-
-        jsonfile["basic analysis"]["missing values in columns"] = {}
-        jsonfile["basic analysis"]["rows with many missing values"] = {}
-
-        dataset_missings_columns = dataset.isna().sum()
-        for index, value in dataset_missings_columns.items():
-
-            jsonfile["basic analysis"]["missing values in columns"][index] = value
-
-        dataset_missings_rows = dataset.isna().sum(axis=1)
-    
-        for index, value in dataset_missings_rows[dataset_missings_rows > len(dataset.columns)*0.5].items():
-
-            jsonfile["basic analysis"]["rows with many missing values"][index] = value
-
+        # map all columns with the number of missing values
+        na_counts = dict(dataset.isna().sum().items())
         
-        return jsonfile
+        return na_counts
 
     @staticmethod
-    def __detect_outliers(dataset: pd.DataFrame, jsonfile: dict) -> dict:
+    def __missing_values_rows(dataset: pd.DataFrame) -> dict:
         """
-        Detects outliers in all columns in a dataset containing floats and stores their indices in a JSON file
+        Counts missing values of each row and adds the indices of rows with a lot of missing values to a list
         ---
         Parameter
         1. dataset to be analyzed
-        2. JSON file to write to
+        ---
+        Return a dictionary containing information about the missing values in rows
+        """
+        dataset_missings_rows = dataset.isna().sum(axis=1)
+
+        missing_rows_indices = []
+
+        number_of_columns = len(dataset.columns)
+
+        for index, _ in dataset_missings_rows[dataset_missings_rows > (number_of_columns * 0.5)].items():
+
+            missing_rows_indices.append(index)
+        
+        return missing_rows_indices
+
+    @staticmethod
+    def __detect_outliers(dataset: pd.DataFrame) -> dict:
+        """
+        Detects outliers in all columns in a dataset containing floats (for now)
+        ---
+        Parameter
+        1. dataset to be analyzed
         ---
         Return the JSON file with additional data
         """
 
-        jsonfile["basic analysis"]["outlier"] = {}
+        outlier_columns = []
 
         dataset_numerics_only = dataset.select_dtypes(include=[np.float])   
     
-        for column in dataset_numerics_only:
+        for column_name in dataset_numerics_only:
             
-            current_column = dataset_numerics_only[column].dropna()
+            current_column = dataset_numerics_only[column_name].copy()
+            current_column = current_column.dropna()
 
             current_column_sorted = current_column.sort_values()
 
-            Q1, Q2, Q3 = np.percentile(current_column_sorted, [25,50,75])
-
-            if ((Q3 - Q1) < 0.1) :
-                continue
+            Q1, Q3 = np.percentile(current_column_sorted, [25,75])
             
-            # interquartile range
-            IQR = Q3 - Q1
+            interquartile_range = Q3 - Q1
 
             # calculation of boundaries is still a work in progress
-            lower_boundary = Q1 - 2 * IQR
-            upper_boundary = Q3 + 2 * IQR
+            lower_boundary = Q1 - 2 * interquartile_range
+            upper_boundary = Q3 + 2 * interquartile_range
 
-            current_column = (current_column < lower_boundary) | (current_column > upper_boundary)
+            current_column_outlier = (current_column < lower_boundary) | (current_column > upper_boundary)
 
-            outlier_indices = current_column[current_column].index.values.tolist()
+            outlier_indices = current_column_outlier[current_column_outlier].index.values.tolist()
 
             outlier_indices = [index + 1 for index in outlier_indices]
 
-            jsonfile["basic analysis"]["outlier"][column] = outlier_indices
+            outlier_columns.append({column_name: outlier_indices})
         
-        return jsonfile
+        return outlier_columns
 
     @staticmethod
     def __detect_duplicate_columns(dataset: pd.DataFrame, jsonfile: dict) -> dict:
