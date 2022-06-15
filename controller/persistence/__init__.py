a11e5083bcc9f3fe985b02311acf80f353986703
Jannik Schleicher <stjischl@stud.h-da.de>
2022-06-15 11:12:32 +0000

    extend docstrings

--- controller/persistence/__init__.py
+++ controller/persistence/__init__.py
@@ -17,6 +17,11 @@ class DataStorage:
         unless `database` is provided.
 
         >>> data_storage = DataStorage("/tmp/")
+
+        ----
+        Parameter
+        1. storage directory on disk
+        2. optional Database object (used for Testing)
         """
         # ensure folder exists
         os.makedirs(data_storage_dir, exist_ok=True)
@@ -35,7 +40,7 @@ class DataStorage:
     def lock(self):
         """
         Lock access to the data storage to a single thread.
-
+        ---
         >>> with data_store.lock():
                 # critical region
                 sess = data_storage.get_session(...)
@@ -48,34 +53,56 @@ class DataStorage:
 
     def insert_session(self, username: str, session: 'dict[str, object]') -> str:
         """
-        Insert single session into the database. Returns session id.
-        
+        Insert single session into the database.
+        ---
         >>> id: str = ds.insert_session("automl_user", {
                 "dataset": ...,
                 ...
             })
+
+        ---
+        Parameter
+        1. username: name of the user
+        2. session: session dict to be inserted
+        ---
+        Returns session id
         """
         return self.__mongo.insert_session(username, session)
 
 
     def get_session(self, username: str, id: str) -> 'dict[str, object]':
         """
-        Get single session by id. Returns session as `dict` or `None` if not found.
-
+        Get single session by id. 
+        ---
         >>> sess = data_storage.get_session("automl_user", sess_id)
         >>> if sess is None:
                 raise Exception("cannot find session")
+        
+        ---
+        Parameter
+        1. username: name of the user
+        2. id: id of session
+        ---
+        Returns session as `dict` or `None` if not found.
         """
         return self.__mongo.get_session(username, id)
 
 
-    def update_session(self, username: str, id: str, new_values : 'dict[str, object]') -> bool:
+    def update_session(self, username: str, id: str, new_values: 'dict[str, object]') -> bool:
         """
-        Update single session with new values. Returns `True` if successfully updated, otherwise `False`.
-        
+        Update single session with new values. 
+        ---
         >>> success: bool = data_storage.update_session("automl_user", sess_id, {
                 "status": "completed"
             })
+
+        ---
+        Parameter
+        1. username: name of the user
+        2. id: id of session
+        3. new_values: dict with new values
+        ---
+        Returns `True` if successfully updated, otherwise `False`.
         """
         return self.__mongo.update_session(username, id, new_values)
 
@@ -83,8 +110,16 @@ class DataStorage:
     def save_dataset(self, username: str, name: str, content: bytes) -> str:
         """
         Store dataset contents on disk and insert entry to database.
-
+        ---
         >>> id: str = ds.save_dataset("automl_user", "my_dataset", ...)
+
+        ---
+        Parameter
+        1. username: name of the user
+        2. name: name of dataset
+        3. content: raw bytes for file on disk
+        ---
+        Returns dataset id
         """
 
         # insert shell entry first to get id from database
@@ -114,11 +149,18 @@ class DataStorage:
 
     def find_dataset(self, username: str, name: str) -> 'tuple[bool, dict[str, object]]':
         """
-        Try to find the _first_ dataset with this name. Returns either `(True, Dataset)` or `(False, None)`.
-
+        Try to find the _first_ dataset with this name. 
+        ---
         >>> found, dataset = ds.find_dataset("automl_user", "my_dataset")
         >>> if not found:
                 print("We have a problem")
+
+        ---
+        Parameter
+        1. username: name of the user
+        2. name: name of dataset
+        ---
+        Returns either `(True, Dataset)` or `(False, None)`.
         """
         result = self.__mongo.find_dataset(username, {
             "name": name
@@ -129,30 +171,45 @@ class DataStorage:
 
     def get_datasets(self, username: str) -> 'list[dict[str, object]]':
         """
-        Get all datasets for a user. Returns `list` of all datasets.
-        
+        Get all datasets for a user. 
+        ---
         >>> for dataset in data_storage.get_datasets("automl_user"):
                 print(dataset["path"])
+
+        ---
+        Parameter
+        1. username: name of the user
+        ---
+        Returns `list` of all datasets.
         """
         return [ds for ds in self.__mongo.get_datasets(username)]
 
 
     def insert_model(self, username: str, model: 'dict[str, object]') -> str:
         """
-        Insert single model into the database. Returns id of new model.
-        
+        Insert single model into the database.
+        ---
         >>> mdl_id: str = data_storage.insert_model("automl_user", {
                 "automl_name": "MLJAR",
                 "session_id": session_id,
                 ...
             })
+
+        ---
+        Parameter
+        1. username: name of the user
+        2. model: dict of model data 
+        ---
+        Returns id of new model.
         """
         return self.__mongo.insert_model(username, model)
 
 
 
     class __DbLock():
-        """DataStore internal helper class. Use with `data_store.lock()`"""
+        """
+        DataStore internal helper class. Use with `data_store.lock()`
+        """
         def __init__(self, inner: Lock):
             self.__inner = inner
             
@@ -161,4 +218,3 @@ class DataStorage:
             
         def __exit__(self, type, value, traceback):
             self.__inner .release()
-
