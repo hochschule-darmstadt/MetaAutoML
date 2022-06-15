173ae4fddd57dc8c91d6317b63c93cc8be2dda30
Jannik Schleicher <stjischl@stud.h-da.de>
2022-06-15 10:27:36 +0000

    Reafctor AdapterManager
    * remove inheritance:  *Manager  -->  AutoMLManager
      * AutoMLManager subclasses are now obsolete
    * collect adapter host and port directly in AdapterManager

--- controller/managers/adaptermanagers/AutoGluonManager.py
+++ controller/managers/adaptermanagers/AutoGluonManager.py
@@ -1,24 +1,24 @@
-from AutoMLManager import AutoMLManager
+# from AutoMLManager import AutoMLManager
 
 
-class AutoGluonManager(AutoMLManager):
-    """
-    Implementation of the specific AutoMLManager for AutoGluon
-    """
-    name = "AutoGluon"
+# class AutoGluonManager(AutoMLManager):
+#     """
+#     Implementation of the specific AutoMLManager for AutoGluon
+#     """
+#     name = "AutoGluon"
 
-    def __init__(self, configuration, folder_location, session_id, callback):
-        """
-        Init a new instance of the specific AutoMLManager
-        ---
-        Parameter
-        1. configuration dictionary
-        2. folder location of the dataset
-        3. session id to use
-        ---
-        Return a new specific AutoML Manager
-        """
-        automl_service_host = 'AUTOGLUON_SERVICE_HOST'
-        automl_service_port = 'AUTOGLUON_SERVICE_PORT'
-        super(AutoGluonManager, self).__init__(configuration, folder_location, automl_service_host, automl_service_port,
-                                               session_id, callback)
+#     def __init__(self, configuration, folder_location, session_id, callback):
+#         """
+#         Init a new instance of the specific AutoMLManager
+#         ---
+#         Parameter
+#         1. configuration dictionary
+#         2. folder location of the dataset
+#         3. session id to use
+#         ---
+#         Return a new specific AutoML Manager
+#         """
+#         automl_service_host = 'AUTOGLUON_SERVICE_HOST'
+#         automl_service_port = 'AUTOGLUON_SERVICE_PORT'
+#         super(AutoGluonManager, self).__init__(configuration, folder_location, automl_service_host, automl_service_port,
+#                                                session_id, callback)
