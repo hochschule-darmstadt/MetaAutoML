e81e6689efc46c59a48ce2f45092a29b6fceb2d5
Igor Alekhnovych <igoraleh@gmail.com>
2022-06-22 22:42:52 +0200

    Updated DatasetManager.cs

--- frontend/src/Server/BlazorBoilerplate.Server/Managers/DatasetManager.cs
+++ frontend/src/Server/BlazorBoilerplate.Server/Managers/DatasetManager.cs
@@ -31,7 +31,47 @@ namespace BlazorBoilerplate.Server.Managers
             _client = client;
             Console.WriteLine("Dataset Manager Created");
         }
+
         /// <summary>
+        /// Retrive all Dataset Types
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ApiResponse> GetDatasetTypes()
+        {
+            GetDatasetTypesResponseDto response = new GetDatasetTypesResponseDto();
+            GetDatasetTypesRequest getDatasetTypesRequest = new GetDatasetTypesRequest();
+            try 
+            {
+                var reply = _client.GetDatasetTypes();
+                response.DatasetTypes = reply.DatasetTypes.ToList();
+                return new ApiResponse(Status200OK, null, response);
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse(Status404NotFound, ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Retrive a selected Dataset Type
+        /// </summary>
+        /// <param name="dataset_type"></param>
+        /// <returns></returns>
+        public async Task<ApiResponse> GetDatasetType(GetDatasetTypeRequestDto dataset_type)    // probably worth renaming
+        {
+            GetDatasetTypeRequest request = new GetDatasetTypeRequest();
+            request.TypeName = dataset_type.TypeName;
+            try
+            {
+                var reply = _client.GetDatasetType(request);
+                return new ApiResponse(Status200OK, null, reply.ReturnCode);
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse(Status404NotFound, ex.Message);
+            }            
+        }
+
         /// Retrive a concrete Dataset
         /// </summary>
         /// <param name="dataset"></param>
@@ -63,6 +103,7 @@ namespace BlazorBoilerplate.Server.Managers
                 return new ApiResponse(Status404NotFound, ex.Message);
             }
         }
+
         /// <summary>
         /// Get a list of all Datasets
         /// </summary>
@@ -89,6 +130,7 @@ namespace BlazorBoilerplate.Server.Managers
                 return new ApiResponse(Status404NotFound, ex.Message);
             }
         }
+
         /// <summary>
         /// Helper function, get all column names of a structured data dataset
         /// </summary>
@@ -112,6 +154,7 @@ namespace BlazorBoilerplate.Server.Managers
                 return new ApiResponse(Status404NotFound, ex.Message);
             }
         }
+
         /// <summary>
         /// Upload a new dataset, currently only CSV are supported
         /// </summary>
---
+++ frontend/src/Shared/BlazorBoilerplate.Shared/Dto/Dataset/GetDatasetTypesResponseDto.cs
@@ -0,0 +1,14 @@
+using BlazorBoilerplate.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBoilerplate.Shared.Dto.Dataset
+{
+    public class GetDatasetTypesResponseDto
+    {
+        public List<string> DatasetTypes { get; set; }
+    }
+}
