ff7cbcc700158bf8dc7ad3f2cd007fe136983fcb
kampmichii <michael.c.merz@stud.h-da.de>
2022-06-30 11:00:51 +0200

    Update AutoGluonAdapter.py
    Nur ein kommentar

--- adapters/AutoGluon/AutoMLs/AutoGluonAdapter.py
+++ adapters/AutoGluon/AutoMLs/AutoGluonAdapter.py
@@ -18,7 +18,32 @@ class AutoGluonAdapter(AbstractAdapter):
         1. Configuration JSON of type dictionary
         """
         super().__init__(configuration)
+
+
+        """
+        self._configuration = configuration
+
+        # set runtime limit from configuration, if it isn't specified its set to 30s
+        if self._configuration["runtime_constraints"]["runtime_limit"] > 0:
+            self._time_limit = self._configuration["runtime_constraints"]["runtime_limit"]
+        else:
+            self._time_limit = 30
+
+        # Interne spalten für Tabellen
+        self._target = self._configuration["tabular_configuration"]["target"]["target"]
+
+
+        # Maximum Itteration set to 3 if 0
+        if self._configuration["runtime_constraints"]["max_iter"] == 0:
+            self._max_iter = self._configuration["runtime_constraints"]["max_iter"] = 3
+
+        # Erstelle den pfad der später verwendet wird.
+        os.mkdir(os.path.join(get_config_property("output-path"), self._configuration["session_id"]))
+
+        """
+
         self._result_path = os.path.join(get_config_property("output-path"), configuration["session_id"], "model_gluon.gluon")
+        # this only sets the result path tbh.
 
     def start(self):
         """
