1aee02d40dda0446f44d1a0b90def50fe1eb4a7d
kampmichii <michael.c.merz@stud.h-da.de>
2022-07-05 22:49:42 +0200

    Implementation of image classification task with custom data_loader
    Komplette Implementierung der image classifizierung backends

--- adapters/AutoGluon/AutoMLs/AutoGluonAdapter.py
+++ adapters/AutoGluon/AutoMLs/AutoGluonAdapter.py
@@ -3,7 +3,10 @@ from autogluon.tabular import TabularDataset, TabularPredictor
 from JsonUtil import get_config_property
 from AbstractAdapter import AbstractAdapter
 from AdapterUtils import read_tabular_dataset_training_data, prepare_tabular_dataset, export_model
+from AutoGluonServer.py import data_loader
+import shutil
 
+from autogluon.vision import ImagePredictor, ImageDataset
 
 class AutoGluonAdapter(AbstractAdapter):
     """
@@ -18,7 +21,32 @@ class AutoGluonAdapter(AbstractAdapter):
         1. Configuration JSON of type dictionary
         """
         super().__init__(configuration)
+
+
+        """
+        self._configuration = configuration
+
+        # set runtime limit from configuration, if it isn't specified its set to 30s
+        if self._configuration["runtime_constraints"]["runtime_limit"] > 0:
+            self._time_limit = self._configuration["runtime_constraints"]["runtime_limit"]
+        else:
+            self._time_limit = 30
+
+        # Interne spalten für Tabellen
+        self._target = self._configuration["tabular_configuration"]["target"]["target"]
+
+
+        # Maximum Itteration set to 3 if 0
+        if self._configuration["runtime_constraints"]["max_iter"] == 0:
+            self._max_iter = self._configuration["runtime_constraints"]["max_iter"] = 3
+
+        # Erstelle den pfad der später verwendet wird.
+        os.mkdir(os.path.join(get_config_property("output-path"), self._configuration["session_id"]))
+
+        """
+
         self._result_path = os.path.join(get_config_property("output-path"), configuration["session_id"], "model_gluon.gluon")
+        # this only sets the result path tbh.
 
     def start(self):
         """
@@ -30,6 +58,8 @@ class AutoGluonAdapter(AbstractAdapter):
             self.__tabular_classification()
         elif self._configuration["task"] == 2:
             self.__tabular_regression()
+        elif self._configuration["task"] == 4:
+            self.__image_classification()
 
     def __tabular_classification(self):
         """
@@ -60,3 +90,26 @@ class AutoGluonAdapter(AbstractAdapter):
             data,
             time_limit=self._time_limit)
         #Fit methode already saves the model
+
+    def __image_classification(self):
+        """
+        Execute the classiciation task
+        """
+        # Daten Laden 
+        train , test = data_loader(self._configuration)
+        
+        # Einteilen 
+        set_hyperparameters={ 
+            'batch_size': self._configuration["test_configuration"]["batch_size"], 
+            'epochs': self._configuration["runtime_constraints"]["epochs"] 
+            }
+        
+        model = ImagePredictor(
+            label=self._target,
+            path=self._result_path)
+        
+         # Trainieren 
+        model.fit(
+            train , 
+            hyperparameters=set_hyperparameters , 
+            time_limit = self._configuration["runtime_constraints"]["runtime_limit"]  ) 
