b7af5105b4b19d99ce60c59e0a14fcd5e774cc2e
istshnabi <shamil.nabiyev@stud.h-da.de>
2022-08-14 15:07:58 +0200

    Updated McflyUtils.py

--- adapters/Mcfly/McflyUtils.py
+++ adapters/Mcfly/McflyUtils.py
@@ -1,29 +1,14 @@
 import json
 import os
-import shutil
-import subprocess
-import sys
-import time
-#import autokeras as ak
-#import tensorflow as tf
-
-import Adapter_pb2
-import Adapter_pb2_grpc
 import dill
-import grpc
 import numpy as np
-import pandas as pd
-from predict_time_sources import DataType, SplitMethod, feature_preparation
 from sklearn.metrics import accuracy_score, mean_squared_error
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sktime.datatypes import convert_to
 from sktime.datasets import load_from_tsfile_to_dataframe
-
-from JsonUtil import get_config_property
-from TemplateGenerator import TemplateGenerator
-import glob
-from PIL import Image
+from sktime.datasets import load_from_tsfile
+from AutoMLs.predict_time_sources import SplitMethod
 
 ######################################################################
 ## LONGITUDINAL DATASET HELPER FUNCTIONS
@@ -32,7 +17,7 @@ from PIL import Image
 #region
 
 
-def split_dataset(dataset, json_configuration):
+def split_dataset(X, y, json_configuration):
     """
     Split the given dataset into train and test subsets
     """
@@ -43,18 +28,18 @@ def split_dataset(dataset, json_configuration):
 
     if int(SplitMethod.SPLIT_METHOD_RANDOM.value) == split_method:
         return train_test_split(
-            dataset,
+            X, y,
             train_size=split_ratio,
             random_state=random_state,
             shuffle=True,
-            stratify=dataset["target"]
+            stratify=y
         )
     else:
         return train_test_split(
-            dataset,
+            X, y,
             train_size=split_ratio,
             shuffle=False,
-            stratify=dataset["target"]
+            stratify=y
         )
 
 
@@ -63,9 +48,10 @@ def read_longitudinal_dataset(json_configuration):
     Read longitudinal data from the `.ts` file
     """
     file_path = os.path.join(json_configuration["file_location"], json_configuration["file_name"])
-    dataset = load_from_tsfile_to_dataframe(file_path, return_separate_X_and_y=False)
-    dataset = dataset.rename(columns={"class_vals": "target"})
-    return split_dataset(dataset, json_configuration)
+    # dataset = load_from_tsfile_to_dataframe(file_path, return_separate_X_and_y=False)
+    # dataset = dataset.rename(columns={"class_vals": "target"})
+    # return split_dataset(dataset, json_configuration)
+    return load_from_tsfile(file_path, return_y=True, return_data_type="numpy3D")
 
 
 def convert_longitudinal_to_numpy(X, y, one_hot_encoder):
@@ -75,5 +61,31 @@ def convert_longitudinal_to_numpy(X, y, one_hot_encoder):
     X_np = convert_to(X, to_type="numpy3D")
     y_binary = one_hot_encoder.transform(y.reshape(-1, 1)).toarray()
     return X_np, y_binary
+
+
+def export_keras_model(model, path, file_name):
+    """
+    Saves the given keras model
+    ---
+    Parameter:
+    1. keras model
+    2. session id
+    3. file name
+    """
+    save_path = os.path.join(path, file_name)
+    model.save(save_path)
+
+
+def export_one_hot_encoder(one_hot_encoder, path, file_name):
+    """
+    Saves the given instance of the sklearn.preprocessing.OneHotEncoder class
+    ---
+    Parameter:
+    1. instance of sklearn.preprocessing.OneHotEncoder
+    2. session id
+    3. file name
+    """
+    with open(os.path.join(path, file_name), 'wb+') as file:
+        dill.dump(one_hot_encoder, file)
 
 #endregion
