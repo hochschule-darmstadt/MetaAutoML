207697ce6c2d9f2d28b62c5543bf62bbcb0308d6
Alexander Zender <alexander.zender@h-da.de>
2022-10-12 17:16:45 +0200

    ported mcfly to new schema, UNTESTED

---
+++ adapters/Mcfly/AutoMLs/McflyAdapterManager.py
@@ -0,0 +1,21 @@
+from AdapterManager import AdapterManager
+import json
+import os
+import time, asyncio
+from AdapterUtils import *
+from AdapterBGRPC import *
+from threading import *
+from JsonUtil import get_config_property
+
+class McflyAdapterManager(AdapterManager):
+    def __init__(self) -> None:
+        super(McflyAdapterManager, self).__init__()
+        self.__automl = None
+        self.__loaded_training_id = None
+
+    def _get_ml_model_and_lib(self, config):
+            # Mcfly is based on the TensorFlow/Keras library
+        return (":tensorflow_lib", ":artificial_neural_network")
+
+    def _load_model_and_make_probabilities(self, config_json, result_folder_location, dataframe):
+        return
