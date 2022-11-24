1308525fa0e6b20fe16e0314a67570e7df3efb11
Eliah Vogel <eliahvogel00@gmail.com>
2022-11-24 11:52:16 +0100

    fixed error on strategy redundant features

--- controller/blackboard/strategies/DataPreparationStrategy.py
+++ controller/blackboard/strategies/DataPreparationStrategy.py
@@ -62,6 +62,7 @@ class DataPreparationStrategyController(IAbstractStrategy):
             raise RuntimeError('Could not access Adapter Runtime Manager Agent!')
         dataset_configuration = json.loads(agent.get_adapter_runtime_manager().get_training_request().dataset_configuration)
 
+        dataset_configuration['features'] = {}
         for column_a, column_b in duplicate_columns:
             self._log.info(f'do_ignore_redundant_features: Encountered redundant feature "{column_b}" (same as "{column_a}"), ingoring the column.')
             dataset_configuration['features'][column_b] = GrpcDataType.DATATYPE_IGNORE
