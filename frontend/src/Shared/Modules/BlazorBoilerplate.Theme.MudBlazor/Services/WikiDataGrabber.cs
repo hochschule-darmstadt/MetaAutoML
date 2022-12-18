a8c6c1378c695894fe40f04eec752b6766c86aa3
Krystofido <Krystofido@users.noreply.github.com>
2022-12-18 21:08:06 +0100

    Created WikiDataGrabber for reusability
    There is now a new .cs file WikiDataGrabber, where all actions in context of Wikidata, Wikipedia, etc. will be happening. This way it will be (re)usable everywhere. For now, all the testing happens in ChipSet.razor

---
+++ frontend/src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Services/WikiDataGrabber.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace BlazorBoilerplate.Theme.Material.Services
+{
+    public class WikiDataGrabber
+    {
+        public string getDataFromLink(string url = null)
+        {
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Accept = "application/json";
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            string result;
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
