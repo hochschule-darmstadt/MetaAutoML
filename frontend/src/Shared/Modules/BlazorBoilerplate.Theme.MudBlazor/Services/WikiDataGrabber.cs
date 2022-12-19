ecdd0569a72e8ad1df3899a6b7376858e509bb38
Krystofido <Krystofido@users.noreply.github.com>
2022-12-19 18:54:13 +0100

    WikiDataGrabber in advanced state
    Cleaned up the code, Wiki-Data-Grabbing should work quite reliably.
    Also came up with a solution to modifiy Wikidata links inside the medthod to contain /entity/ instead /wiki/ to not have to modify an enormous amount of the .ttl file.

--- frontend/src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Services/WikiDataGrabber.cs
+++ frontend/src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Services/WikiDataGrabber.cs
@@ -1,12 +1,21 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace BlazorBoilerplate.Theme.Material.Services
 {
     public class WikiDataGrabber
     {
-        public string getDataFromLink(string url = null)
+        public string getDataFromUrl(string url, string language = "en")
         {
+            if(!isWikiDatalink(url))
+            {
+                return url;
+            }
+            
+            url = adjustUrl(url);
+
             var httpRequest = (HttpWebRequest)WebRequest.Create(url);
             httpRequest.Accept = "application/json";
             var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
@@ -16,19 +25,56 @@ namespace BlazorBoilerplate.Theme.Material.Services
                 result = streamReader.ReadToEnd();
             }
 
-            dynamic stuff = JsonConvert.DeserializeObject(result);
-
-            var test1 = stuff.entities; // access element entities
-
-            var test3 = url.Split('/').Last(); // extract uri from url
-
-            var test4 = stuff.entities[test3]; // access elements/uri-which-is-just-a-string
-
-
-            string description = stuff.entities[test3].descriptions.en.value;
-
+            dynamic jsonObject = stringToJsonObject(result);
+            string uri = extractUri(url);
+            string description = getWikidataDescription(jsonObject, uri);
 
             return description;
+        }
+
+        // Deserialize the JSON String to a JSON Object
+        public dynamic stringToJsonObject(string data)
+        {
+            dynamic jsonObject = JsonConvert.DeserializeObject(data);
+            return jsonObject;
+        }
+
+        // The URI will be needed to access the correct part of the JSON
+        public string extractUri(string url)
+        {
+            string uri = url.Split('/').Last();
+            return uri;
+        }
+
+        // if not stated otherwise, take the english description
+        public string getWikidataDescription(dynamic jsonObject, string uri, string language = "en")
+        {
+            string description = jsonObject.entities[uri].descriptions[language].value;
+
+            return description;
+        }
+
+        // Simple Regex - if the url is already correct, nothing bad will happen. Wikidata Url needs to contain
+        // /entity/ instead of /wiki/ to return a JSON 
+        public string adjustUrl(string url)
+        {
+            string adjustedUrl = Regex.Replace(url, "/wiki/", "/entity/");
+            return adjustedUrl;
+        }
+
+        // Adds a little fail-safety. Why this method exists: While testing with ChipSet.razor I noticed, that a
+        // "chiptoadd" was not really a chip. Its tooltip text was
+        // "This strategy ignores certain dataset columns if they have been flagged as duplicate in the dataset analysis."
+        // I don't know the reason why this happens, so I added this failsafe instead.
+        // Update: In case of Chips it seems that sometimes they already have a tooltip. This failsafe might turn out
+        // really helpful in other cases too.
+        public bool isWikiDatalink(string url)
+        {
+            if(Regex.IsMatch(url, "https://www.wikidata.org/"))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
