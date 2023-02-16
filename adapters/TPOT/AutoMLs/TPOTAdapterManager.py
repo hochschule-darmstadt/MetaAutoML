5c2892b05727af94cd60f6265ac176e5d128f087
AlexanderZender <79153884+AlexanderZender@users.noreply.github.com>
2023-02-16 13:58:28 +0100

    Adapter tpot (#306)
    * Added new TPOT-Adapter with Template of AutoKeras
    
    * change files for preparing to implement TPOT Adapter
    
    * Added requirements for TPOT and started to implement tabular classification
    
    ---------
    
    Co-authored-by: Julius <brelljulius@gmail.com>
    Co-authored-by: Pia Friehe <pia.friehe@web.de>

---
+++ adapters/TPOT/AutoMLs/TPOTAdapterManager.py
@@ -0,0 +1,64 @@
+from AdapterManager import AdapterManager
+import json
+import os
+import time, asyncio
+from AdapterUtils import *
+from AdapterBGRPC import *
+from threading import *
+from JsonUtil import get_config_property
+import pandas as pd
+from typing import Tuple
+
+class TPOTAdapterManager(AdapterManager):
+    """The AutoML solution specific functionality implementation of the AdapterManager class
+
+    Args:
+        AdapterManager (AdapterManager): The base class providing the shared functionality for all adapters
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new TPOTAdapterManager setting AutoML adapter specific variables
+        """
+        super(TPOTAdapterManager, self).__init__()
+        self.__automl = None
+        self.__loaded_training_id = None
+        self._adapter_name = "tpot"
+
+    def _get_ml_model_and_lib(self, config: "StartAutoMlRequest") -> Tuple[str, str]:
+        """Get the ML model type and ml library used by the result model
+
+        Args:
+            config (StartAutoMlRequest): extended Start AutoMlRequest holding the training configuration and local training paths
+
+        Returns:
+            tuple[str, str]: Tuple returning the ontology IRI of the Ml model type, and ontology IRI of the ML library
+        """
+        return (":keras_lib", ":artificial_neural_network")
+
+    def _load_model_and_make_probabilities(self, config: "StartAutoMlRequest", result_folder_location: str, dataframe: pd.DataFrame):
+        """Must be overwriten! Load the found model, and execute a prediction using the provided data to calculate the probability metric used by the ExplanableAI module inside the controller
+
+        Args:
+            config (StartAutoMlRequest): extended Start AutoMlRequest holding the training configuration and local training paths
+            result_folder_location (str): The absolute path leading to the model result location
+            dataframe (DataFrame): The dataframe holding the dataset to execute a new prediction on
+        """
+        # Check if the requested training is already loaded. If not: Load model and load & prep dataset.
+        if self._loaded_training_id != config["training_id"]:
+            print(f"ExplainModel: Model not already loaded; Loading model")
+            with open(result_folder_location + '/model_keras.p', 'rb') as file:
+                self.__automl = dill.load(file)
+                # Export model as AutoKeras does not provide the prediction probability.
+                self.__automl = self.__automl.export_model()
+            self._loaded_training_id = config["training_id"]
+            # Get prediction probabilities and send them back.
+        probabilities = self.__automl.predict(np.array(dataframe.values.tolist()))
+        # Keras is strange as it does not provide a predict_proba() function to get the class probabilities.
+        # Instead, it returns these probabilities (in case there is a binary classification) when calling predict
+        # but only as a one dimensional array. Shap however requires the probabilities in the format
+        # [[prob class 0, prob class 1], [...]]. So to return the proper format we have to process the results of
+        # predict().
+        if probabilities.shape[1] == 1:
+            probabilities = [[1 - prob[0], prob[0]] for prob in probabilities.tolist()]
+        probabilities = json.dumps(probabilities)
+        return probabilities
