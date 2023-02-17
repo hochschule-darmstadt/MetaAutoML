02c19147be03188b181f43f652defc625c79cb74
AlexanderZender <alexander.zender@h-da.de>
2023-02-17 11:20:05 +0100

    TPOT classification working

--- adapters/TPOT/AutoMLs/TPOTAdapter.py
+++ adapters/TPOT/AutoMLs/TPOTAdapter.py
@@ -1,6 +1,6 @@
 import numpy as np
 from AdapterUtils import export_model, prepare_tabular_dataset, data_loader
-from tpot import TPOTClassifier
+from tpot import TPOTClassifier, TPOTRegressor
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 
@@ -35,8 +35,14 @@ class TPOTAdapter:
         pipeline_optimizer = TPOTClassifier(generations=5, population_size=20, cv=5,
                                             random_state=42, verbosity=2, max_time_mins=self._configuration["configuration"]["runtime_limit"]*60)
         pipeline_optimizer.fit(X, y)
-        export_model(pipeline_optimizer, self._configuration["result_folder_location"], 'model_TPOT.p')
+        export_model(pipeline_optimizer.fitted_pipeline_, self._configuration["result_folder_location"], 'model_TPOT.p')
 
 
     def __tabular_regression(self):
-        pass
+        self.df, test = data_loader(self._configuration)
+        X, y = prepare_tabular_dataset(self.df, self._configuration)
+
+        pipeline_optimizer = TPOTRegressor(generations=5, population_size=20, cv=5,
+                                            random_state=42, verbosity=2, max_time_mins=self._configuration["configuration"]["runtime_limit"]*60)
+        pipeline_optimizer.fit(X, y)
+        export_model(pipeline_optimizer, self._configuration["result_folder_location"], 'model_TPOT.p')
