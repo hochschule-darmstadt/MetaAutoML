561efd27680b79f231313bd22f4dfc59c98fdf98
AlexanderZender <alexander.zender@h-da.de>
2023-02-19 16:56:32 +0100

    updated pycaret to include all estimators and regression

--- adapters/PyCaret/AutoMLs/PyCaretAdapter.py
+++ adapters/PyCaret/AutoMLs/PyCaretAdapter.py
@@ -1,7 +1,7 @@
 import os
 
 from AdapterUtils import export_model, prepare_tabular_dataset, data_loader, get_column_with_largest_amout_of_text
-from pycaret.classification import *
+
 import numpy as np
 from sklearn.impute import SimpleImputer
 import pandas as pd
@@ -38,33 +38,25 @@ class PyCaretAdapter:
 
     def __tabular_classification(self):
         """Execute the tabular classification task and export the found model"""
+        from pycaret.classification import setup, compare_models, save_model
+
         self.df, test = data_loader(self._configuration)
         X, y = prepare_tabular_dataset(self.df, self._configuration)
         X[y.name] = y
         automl = setup(data = X, target = y.name)
-        best = compare_models()
-        dt = create_model(best)
-        tuned_dt = tune_model(dt)
-        final_dt = finalize_model(tuned_dt)
-        save_model(final_dt, os.path.join(self._configuration["result_folder_location"], 'model_pycaret'))
+        best = compare_models(budget_time=self._configuration["configuration"]["runtime_limit"] * 60)
+        save_model(best, os.path.join(self._configuration["result_folder_location"], 'model_pycaret'))
         #export_model(automl, self._configuration["result_folder_location"], 'model_pycaret.p')
 
     def __tabular_regression(self):
         """Execute the tabular regression task and export the found model"""
+        from pycaret.regression import setup, compare_models, save_model
         self.df, test = data_loader(self._configuration)
         X, y = prepare_tabular_dataset(self.df, self._configuration)
-        automl = AutoML()
-        automl_settings = self.__generate_settings()
-        automl_settings.update({
-            #"metric": self._configuration["configuration"]["metric"] if self._configuration["configuration"]["metric"] != "" else 'accuracy',
-            "metric": 'mse',
-            "task": 'regression',
-            "log_file_name": self._log_file_path
-        })
-
-        X[y.name] = y.values
-        automl.fit(dataframe=X, label=y.name, **automl_settings)
-        export_model(automl, self._configuration["result_folder_location"], 'model_pycaret.p')
+        X[y.name] = y
+        automl = setup(data = X, target = y.name)
+        best = compare_models(budget_time=self._configuration["configuration"]["runtime_limit"] * 60)
+        save_model(best, os.path.join(self._configuration["result_folder_location"], 'model_pycaret'))
 
 
     def __time_series_forecasting(self):
