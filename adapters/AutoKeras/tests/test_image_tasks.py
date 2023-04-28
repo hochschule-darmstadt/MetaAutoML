a07510aaed0b69d438760634aef5ae428c74b48b
Pia Friehe <pia.friehe@web.de>
2023-04-28 08:27:09 +0200

    configuration test image tasks adapted

--- adapters/AutoKeras/tests/test_image_tasks.py
+++ adapters/AutoKeras/tests/test_image_tasks.py
@@ -84,9 +84,13 @@ class AutoKerasImageTaskTest(unittest.TestCase):
         req.configuration.target = ""
         req.configuration.runtime_limit = 1
         req.configuration.metric = ':accuracy'
-        req.configuration.parameters = {":metric": {"values": [":mean_sqared_error"]}}
+        req.configuration.parameters = {":metric": {"values": [":mean_squared_error"]}}
         # we do not need a dataset configuration
-        req.dataset_configuration = json.dumps({"multi_fidelity_level": 0})
+        req.dataset_configuration = json.dumps({
+            "file_configuration": {},
+            "schema": {},
+            "multi_fidelity_level": 0
+        })
 
         adapter_manager = AutoKerasAdapterManager()
         adapter_manager.start_auto_ml(req, uuid.uuid4())
@@ -119,40 +123,12 @@ class AutoKerasImageTaskTest(unittest.TestCase):
         req.configuration.metric = ':accuracy'
         req.configuration.parameters = {":metric": {"values": [":accuracy"]}}
         # we do not need a dataset configuration
-        req.dataset_configuration = json.dumps({"multi_fidelity_level": 0})
+        req.dataset_configuration = json.dumps({
+            "file_configuration": {},
+            "schema": {},
+            "multi_fidelity_level": 0
+        })
 
-        adapter_manager = AutoKerasAdapterManager()
-        adapter_manager.start_auto_ml(req, uuid.uuid4())
-        adapter_manager.start()
-        adapter_manager.join()
-
-        # check if model archive exists
-        out_dir = os.path.join("app-data", "training",
-                               req.user_id, req.dataset_id, req.training_id)
-        path_to_model = os.path.join(out_dir, "export", "keras-export.zip")
-        self.assertTrue(os.path.exists(path_to_model), f"path to model: '{path_to_model}' does not exist")
-
-        # clean up
-        shutil.rmtree(out_dir)
-
-    def test_parameters_image_classification(self):
-
-        dataset_path = load_mnist_image_dataset()
-
-        req = StartAutoMlRequest()
-        req.training_id = "test"
-        req.dataset_id = "test"
-        req.user_id = "test"
-        req.dataset_path = dataset_path
-        req.configuration.task = ':image_classification'
-        # we do not need a target, it will be ignored,
-        #   the default is None, which will raise an error
-        req.configuration.target = ""
-        req.configuration.runtime_limit = 1
-        req.configuration.metric = ':accuracy'
-        req.configuration.parameters = {":metric": {"values": [":average_precision_score"]}}
-        # we do not need a dataset configuration
-        req.dataset_configuration = json.dumps({"multi_fidelity_level": 0})
 
         adapter_manager = AutoKerasAdapterManager()
         adapter_manager.start_auto_ml(req, uuid.uuid4())
