8df5e540fe67cb23042f16362da6d6e2eec5ea0e
Tim Rooney <timrooney68@gmail.com>
2023-05-09 20:31:38 +0200

    #325: Ignore certificate errors when talking to OMAML (in development, the controller uses a self-signed certificate)

--- benchmark-tool/src/grpc_omaml/client_factory.py
+++ benchmark-tool/src/grpc_omaml/client_factory.py
@@ -1,6 +1,7 @@
 from grpc_omaml import ControllerServiceStub
 from grpclib.client import Channel
 from config.config_accessor import get_omaml_server_host, get_omaml_server_port
+from ssl import SSLContext, PROTOCOL_TLSv1_2, CERT_NONE
 
 
 def create_omaml_client():
@@ -9,5 +10,21 @@ def create_omaml_client():
     Returns:
         ControllerServiceStub: The omaml client
     """
-    channel = Channel(host=get_omaml_server_host(), port=get_omaml_server_port())
+    channel = Channel(
+        host=get_omaml_server_host(),
+        port=get_omaml_server_port(),
+        ssl=get_ignore_certificate_config(),
+    )
     return ControllerServiceStub(channel=channel)
+
+
+def get_ignore_certificate_config():
+    """Creates a ssl configuration that ignores the certificate.
+
+    Returns:
+        ssl configuration
+    """
+    ssl = SSLContext(PROTOCOL_TLSv1_2)
+    ssl.check_hostname = False
+    ssl.verify_mode = CERT_NONE
+    return ssl
