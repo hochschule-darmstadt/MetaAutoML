8756f1d10b09e2c521ebc97d35ca761270b07da0
Julia <julia@DESKTOP-VCTLHFA.localdomain>
2023-05-31 18:11:52 +0200

    funktioniert mit wsl

--- adapters/FLAML/tests/test_explainer_dashboard.py
+++ adapters/FLAML/tests/test_explainer_dashboard.py
@@ -4,57 +4,49 @@ import shutil
 import unittest
 import uuid
 
-import keras
-import pandas as pd
 from AdapterBGRPC import StartAutoMlRequest
 from FLAMLAdapterManager import FLAMLAdapterManager
-from ControllerBGRPC import DataType
+from sklearn.datasets import load_iris
+import pandas as pd
 
+def load_iris_dataset() -> str:
+    """download iris dataset and build csv file, return csv path"""
 
-def load_titanic_dataset():
-    """download titanic dataset and return path of reduced dataset csv"""
-    cache_dir = os.path.join("tests", "datasets")
-    os.makedirs(cache_dir, exist_ok=True)
-    dataset_path = keras.utils.get_file(
-        fname="titanic_full.csv",
-        origin="https://storage.googleapis.com/tf-datasets/titanic/train.csv",
-        cache_dir=cache_dir,
-        cache_subdir=""
-    )
+    #load dataset
+    iris = load_iris()
+    train_data = pd.DataFrame(iris['data'], columns=iris['feature_names'])
+    train_data['target'] = iris['target']
+    # save dataset to file
+    os.makedirs(os.path.join("tests", "datasets"), exist_ok=True)
+    file_path = os.path.join("tests", "datasets", "iris.csv")
 
-    titanic: pd.DataFrame = pd.read_csv(dataset_path)
+    train_data.to_csv(file_path, index=False)
+    return file_path
 
-    # drop other columns for simplicity
-    titanic = titanic[["age", "sex", "survived"]]
+class FLAMLTabularTaskTest(unittest.TestCase):
 
-    # dp not overwrite downloaded from the internet for caching
-    dataset_path = os.path.join(os.path.dirname(dataset_path), "titanic.csv")
-    with open(dataset_path, "w+") as outfp:
-        pd.DataFrame.to_csv(titanic, outfp, index=False)
-
-    return dataset_path
-
-class FLAMLExplainerDashboardTest(unittest.TestCase):
     def test_tabular_classification(self):
 
-        dataset_path = load_titanic_dataset()
+        dataset_path = load_iris_dataset()
 
-        # setup request as it is coming in from controller
         req = StartAutoMlRequest()
         req.training_id = "test"
         req.dataset_id = "test"
         req.user_id = "test"
         req.dataset_path = dataset_path
         req.configuration.task = ':tabular_classification'
-        req.configuration.target = "survived"
-        req.configuration.runtime_limit = 1
+        req.configuration.target = "target"
+        req.configuration.runtime_limit = 3
         req.configuration.metric = ':accuracy'
         req.configuration.parameters = {":metric": {"values": [":accuracy"]}}
         req.dataset_configuration = json.dumps({
             "column_datatypes": {
-                "age": DataType.DATATYPE_INT,
-                "sex": DataType.DATATYPE_STRING,
-                "survived": DataType.DATATYPE_INT,
+                "target": ":integer",
+                "sepal length (cm)": ":float",
+                "sepal width (cm)": ":float",
+                "petal length (cm)": ":float",
+                "petal width (cm)": ":float"
+
             },
             "file_configuration": {
                 "use_header": True,
@@ -64,20 +56,73 @@ class FLAMLExplainerDashboardTest(unittest.TestCase):
                 "decimal_character": ".",
                 "thousands_seperator": ",",
                 "datetime_format": "",
-                "thousands_seperator": ",",
-                "datetime_format": "",
-                "encoding": ""
+                "encoding": "utf-8"
             },
             "schema": {
-                "survived": {
-                    "datatype_detected": ":boolean",
+                "target": {
+                    "datatype_detected": ":int",
                     "role_selected": ":target"
                 }
             },
             "multi_fidelity_level": 0
         })
 
-        # start training
+        adapter_manager = FLAMLAdapterManager()
+        adapter_manager.start_auto_ml(req, uuid.uuid4())
+        adapter_manager.start()
+        adapter_manager.join()
+
+        # check if model archive exists
+        out_dir = os.path.join("app-data", "training",
+                               req.user_id, req.dataset_id, req.training_id)
+        path_to_model = os.path.join(out_dir, "export", "flaml-export.zip")
+        self.assertTrue(os.path.exists(path_to_model), f"path to model: '{path_to_model}' exist")
+
+        # clean up
+        shutil.rmtree(out_dir)
+
+    def test_tabular_regression(self):
+
+        dataset_path = load_iris_dataset()
+
+        req = StartAutoMlRequest()
+        req.training_id = "test"
+        req.dataset_id = "test"
+        req.user_id = "test"
+        req.dataset_path = dataset_path
+        req.configuration.task = ':tabular_regression'
+        req.configuration.target = "target"
+        req.configuration.runtime_limit = 2
+        req.configuration.metric = ':accuracy'
+        req.configuration.parameters = {":metric": {"values": [":mean_squared_error"]}}
+        req.dataset_configuration = json.dumps({
+            "column_datatypes": {
+                "target": ":integer",
+                "sepal length (cm)": ":float",
+                "sepal width (cm)": ":float",
+                "petal length (cm)": ":float",
+                "petal width (cm)": ":float"
+
+            },
+            "file_configuration": {
+                "use_header": True,
+                "start_row": 1,
+                "delimiter": "comma",
+                "escape_character": "\\",
+                "decimal_character": ".",
+                "thousands_seperator": ",",
+                "datetime_format": "",
+                "encoding": "utf-8"
+            },
+            "schema": {
+                "target": {
+                    "datatype_detected": ":int",
+                    "role_selected": ":target"
+                }
+            },
+            "multi_fidelity_level": 0
+        })
+
         adapter_manager = FLAMLAdapterManager()
         adapter_manager.start_auto_ml(req, uuid.uuid4())
         adapter_manager.start()
