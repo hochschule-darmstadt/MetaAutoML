1b50fe8991e80c266b88eabab46b2dc282e59f9a
Tim Rooney <timrooney68@gmail.com>
2023-06-10 10:41:27 +0200

    #327: Call column configuration in main, update documentation of grpc, change dataset name to not conflict with the default example

--- benchmark-tool/src/main.py
+++ benchmark-tool/src/main.py
@@ -1,6 +1,6 @@
 from user.user import init_user
 from dataset.dataset_configuration_reader import read_dataset_configuration
-from dataset.dataset import upload_dataset
+from dataset.dataset import configure_dataset, upload_dataset
 from config.config_validator import validate_config_values
 from asyncio import run
 from grpc_omaml.omaml_client import OmamlClient
@@ -13,11 +13,13 @@ async def main():
         return
     with OmamlClient() as client:
         userId = await init_user(client)
+        print(f"User id: {userId}")
         dataset_configurations = read_dataset_configuration()
 
         # TODO: parallelize
         for dataset_config in dataset_configurations:
-            await upload_dataset(client, userId, dataset_config)
+            datasetId = await upload_dataset(client, userId, dataset_config)
+            await configure_dataset(client, userId, datasetId, dataset_config)
 
 
 if __name__ == "__main__":
