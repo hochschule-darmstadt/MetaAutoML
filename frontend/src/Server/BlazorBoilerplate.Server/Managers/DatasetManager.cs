7148dfb5fd776816c7dc866873604322ebc0d609
Dong Hung Pham <donghungcp98@gmail.com>
2023-11-22 15:25:35 +0100

    update dto for new property ydataprofilling report path

--- frontend/src/Server/BlazorBoilerplate.Server/Managers/DatasetManager.cs
+++ frontend/src/Server/BlazorBoilerplate.Server/Managers/DatasetManager.cs
@@ -341,42 +341,11 @@ namespace BlazorBoilerplate.Server.Managers
                 var analysis = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(reply.Dataset.Analysis);
                 int index = 0;
                 string graph_path = "";
+                // check if analysis for yprofilling exist, set to the bool in dto
                 if (analysis != null)
                 {
-                    if (analysis.Count != 0)
-                    {
-                        if (analysis["plots"] != null)
-                        {
-                            foreach (var category in analysis["plots"])
-                            {
-                                DatasetAnalysisCategory datasetAnalysisCategory = new DatasetAnalysisCategory()
-                                {
-                                    CategoryTitle = category.SelectToken("title")
-                                };
-                                foreach (var item in category["items"])
-                                {
-                                    if (request.GetShortPreview == true && index++ == 3)
-                                    {
-                                        break;
-                                    }
-                                    graph_path = item.SelectToken("path").ToString();
-                                    if (is_mutagen_setup == "YES")
-                                    {
-                                        graph_path = graph_path.Replace(mutagen_docker_ataset_folder_path, mutagen_dataset_folder_path);
-                                    }
-                                    DatasetAnalysis datasetAnalysis = new DatasetAnalysis()
-                                    {
-                                        Title = item.SelectToken("title").ToString(),
-                                        Type = item.SelectToken("type").ToString(),
-                                        Description = item.SelectToken("description").ToString(),
-                                        Content = GetImageAsBytes(graph_path)
-                                    };
-                                    datasetAnalysisCategory.Analyses.Add(datasetAnalysis);
-                                }
-                                response.AnalysisCategories.Add(datasetAnalysisCategory);
-                            }
-                        }
-                    }
+                    if (analysis.ContainsKey("report_path")) response.ydataprofilling = analysis["report_path"];
+                    else response.ydataprofilling = null;
                 }
                 return new ApiResponse(Status200OK, null, response);
 
