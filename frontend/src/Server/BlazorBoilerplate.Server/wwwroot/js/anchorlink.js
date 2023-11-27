4973c133514a600dc1d6837aa6269925192a9d6d
Alexander Zender <alexander.zender@h-da.de>
2023-11-27 10:21:59 +0100

    added anchorlink support

---
+++ frontend/src/Server/BlazorBoilerplate.Server/wwwroot/js/anchorlink.js
@@ -0,0 +1,17 @@
+
+
+window.anchorLink = {
+    init: function (anchorId) {
+        var elem = document.getElementById(anchorId);
+        if (elem) {
+            elem.setAttribute("onclick", "return false;");
+        }
+    },
+    scrollIntoView: function (elementId) {
+        var elem = document.getElementById(elementId);
+        if (elem) {
+            elem.scrollIntoView();
+            window.location.hash = elementId;
+        }
+    }
+};
