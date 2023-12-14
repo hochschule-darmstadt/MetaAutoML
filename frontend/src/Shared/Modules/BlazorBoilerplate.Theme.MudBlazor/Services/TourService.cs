99ae67e76f11c8f96668e23ed8c65e958acc3fc7
AnnaHlzhsr <anna.holzheuser@stud.h-da.de>
2023-12-14 17:20:26 +0100

    User Tour integrated
    Added the user tour to all necessary components.

--- frontend/src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Services/TourService.cs
+++ frontend/src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Services/TourService.cs
@@ -2,16 +2,20 @@ using System;
 using Microsoft.AspNetCore.Components;
 using System.Net.NetworkInformation;
 using GTour.Abstractions;
-
+using MudBlazor;
+using BlazorBoilerplate.Theme.Material.Shared.Components;
 
 namespace BlazorBoilerplate.Theme.Material.Services
 {
     public class TourService
     {
+        private readonly NavigationManager _navigationManager;
         private readonly IGTourService _gTourService;
-        public TourService(IGTourService gTourService)
+        public TourService(IGTourService gTourService, NavigationManager navigationManager)
         {
             _gTourService = gTourService;
+            _navigationManager = navigationManager;
+
         }
         
         private string GetQueryParam(string paramName, string uri)
@@ -21,14 +25,39 @@ namespace BlazorBoilerplate.Theme.Material.Services
             return q[paramName] ?? "";
         }
 
+        public bool checkIfUserTourIsActivated(string uri)
+        {
+            string queryParam = this.GetQueryParam("TourStep", uri);
+            if (!string.IsNullOrEmpty(queryParam))
+            {
+                return true;
+            }
+            else { return false; }
+        }
 
-        public async Task checkIfUserTourIsActivated(string uri)
+
+        public async Task checkIfUserTourIsActivatedAndStartTour(string uri)
         {
             string queryParam = this.GetQueryParam("TourStep", uri);
             if (!string.IsNullOrEmpty(queryParam))
             {
                 await _gTourService.StartTour("FormGuidedTour", queryParam);
             }
+        }
+        public void navigateToNextPage(string step, string uri)
+        {
+            // var uri = navigationManager.GetUriWithQueryParameter("TourActivated", true);
+            //var shouldUri = navigationManager.ToAbsoluteUri(" ").AbsoluteUri;
+            if (uri.EndsWith("/")) { uri = uri.Remove(uri.Length - 1, 1); }
+            var uriBuilder = new UriBuilder(uri);
+            var q = System.Web.HttpUtility.ParseQueryString(uriBuilder.Query);
+            q["TourStep"] = step;
+            uriBuilder.Query = q.ToString();
+            var newUrl = uriBuilder.ToString();
+            _navigationManager.NavigateTo(newUrl);
+            //await context.NextStep();
+            //await GTourService.StartTour("FormGuidedTour", "forthStep");
+
         }
     }
 }
