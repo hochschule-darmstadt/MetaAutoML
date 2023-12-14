c766148d7fae0f5b786762f24bf21393fe254ad0
pdotter <40352664+pdotter@users.noreply.github.com>
2023-12-14 09:31:20 +0100

    Start updating adapter files

--- adapters/H2O/AutoMLs/H2OAdapter.py
+++ adapters/H2O/AutoMLs/H2OAdapter.py
@@ -14,11 +14,11 @@ from h2o.automl import H2OAutoML
 # # Laden Sie Ihre Daten in ein H2O Frame
 # df = h2o.import_file("your_data.csv")
 
-# # Für eine Klassifikation: Stellen Sie sicher, dass Ihre Zielvariable als Faktor (also als kategorisch) gesetzt ist, bevor Sie mit dem Training beginnen. Dies können Sie mit der asfactor() Methode tun:
+# # Fï¿½r eine Klassifikation: Stellen Sie sicher, dass Ihre Zielvariable als Faktor (also als kategorisch) gesetzt ist, bevor Sie mit dem Training beginnen. Dies kï¿½nnen Sie mit der asfactor() Methode tun:
 # target = 'your_target_column'
 # df[target] = df[target].asfactor()  # Konvertiert die Zielvariable in einen Faktor, wenn es sich um eine Klassifikation handelt
 
-# # Für eine Regression: Stellen Sie sicher, dass Ihre Zielvariable als numerisch gesetzt ist.
+# # Fï¿½r eine Regression: Stellen Sie sicher, dass Ihre Zielvariable als numerisch gesetzt ist.
 # target = 'your_target_column'
 # df[target] = df[target].asnumeric() # Konvertiert die Zielvariable in Numeric, wenn es sich um eine Regression handelt
 
@@ -26,7 +26,7 @@ from h2o.automl import H2OAutoML
 # h2o_df = h2o.H2OFrame(df)
 
 
-class h2oAdapter:
+class H2OAdapter:
     """
     Implementation of the AutoML functionality for H2O
     """
@@ -39,7 +39,7 @@ class h2oAdapter:
         self._configuration = configuration
         h2o.init()
         if self._configuration["configuration"]["runtime_limit"] > 0:
-            self._time_limit = self._configuration["configuration"]["runtime_limit"]
+            self._time_limit = self._configuration["configuration"]["runtime_limit"] * 60
         else:
             self._time_limit = 30
 
@@ -55,17 +55,20 @@ class h2oAdapter:
     def __tabular_classification(self):
         """Execute the tabular classification task and export the found model"""
 
-        self.df, test = h2o.import_file(data_loader(self._configuration))
+        self.df, test = data_loader(self._configuration)
         features, targets = prepare_tabular_dataset(self.df, self._configuration, apply_feature_extration=True)
-        df[target].asfactor()
-        parameters = translate_parameters(":h2o", self._configuration["configuration"]["task"], self._configuration["configuration"].get('parameters', {}), h2opc.parameters)
-        parameters.update({"max_trials": self._configuration["configuration"]["runtime_limit"]})
-        aml = H2OAutoML(max_runtime_secs = 35, seed = 1)
-        aml.train(y = h2o.H2OFrame(targets), training_frame = h2o.H2OFrame(features))
+
+        #df[target].asfactor()
+        parameters = translate_parameters(":h2o_automl", self._configuration["configuration"]["task"], self._configuration["configuration"].get('parameters', {}), h2opc.parameters)
+        pandasJoineddf = pd.concat([features, targets], axis=1)
+
+        aml = H2OAutoML(max_runtime_secs = self._time_limit, seed = 1)
+        aml.train(y = targets.name, training_frame = h2o.H2OFrame(pandasJoineddf))
         # The leader model is stored here
         # View the AutoML Leaderboard
         lb = aml.leaderboard
         lb.head(rows=lb.nrows)  # Print all rows instead of default (10 rows)
+        #export
 
 
 
