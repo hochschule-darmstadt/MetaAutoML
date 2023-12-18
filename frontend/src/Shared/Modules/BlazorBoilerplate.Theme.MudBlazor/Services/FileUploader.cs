1f6f3cdfdcb0ee6a5fdd7f9fa4f47e13d3ababa2
Amad Ahmed <69305541+amadr@users.noreply.github.com>
2023-12-18 20:51:38 +0100

    Add openml url download functionality
    Add get request to openml for retrieving direct download url

--- frontend/src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Services/FileUploader.cs
+++ frontend/src/Shared/Modules/BlazorBoilerplate.Theme.MudBlazor/Services/FileUploader.cs
@@ -16,7 +16,7 @@ using BlazorBoilerplate.Shared.Dto.Prediction;
 //using HtmlAgilityPack;
 using static Microsoft.AspNetCore.Http.StatusCodes;
 using System.Net;
-
+using System.Text.Json;
 
 namespace BlazorBoilerplate.Theme.Material.Services
 {
@@ -215,6 +215,10 @@ namespace BlazorBoilerplate.Theme.Material.Services
             {
                 return GetNextcloudDownloadLink(url);
             }
+            if (Regex.IsMatch(url, @"(?:openml\.org/search).*type=data"))
+            {
+                return GetOpenMLDownloadLink(url);
+            }
             return url;
         }
         private static string GetGoogleDriveDownloadLink(string googleDriveUrl)
@@ -267,6 +271,34 @@ namespace BlazorBoilerplate.Theme.Material.Services
                 return nextcloudUrl;
             }
             return nextcloudUrl + "/download";
+        }
+
+        private static string GetOpenMLDownloadLink(string openMLUrl)
+        {
+            var match = Regex.Match(openMLUrl, @"(?:\b|\?)id=(\d+)\b");
+            if (match.Success)
+            {
+                string datasetId = match.Groups[1].Value;
+                string apiUrl = $"https://www.openml.org/api/v1/json/data/{datasetId}";
+                try
+                {
+                    using (var client = new HttpClient())
+                    {
+                        HttpResponseMessage response = client.GetAsync(apiUrl).Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            JsonDocument jsonDocument = JsonDocument.Parse(response.Content.ReadAsStringAsync().Result);
+                            JsonElement urlElement = jsonDocument.RootElement.GetProperty("data_set_description").GetProperty("url");
+                            openMLUrl = urlElement.GetString();
+                            return openMLUrl;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return openMLUrl;
         }
     }
 }
