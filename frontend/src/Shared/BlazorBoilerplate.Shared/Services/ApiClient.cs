92d4ca4b226a2205ca765b5875c28d475cb35595
Anna Holzheuser <116272018+AnnaHlzhsr@users.noreply.github.com>
2023-12-31 14:58:06 +0100

    Ux improvement help site (#443)
    * Included Help Site
    
    Included Help Site in NavMenu and integrated breadcrumbs and headlines
    
    * Added Images, Videos and GIFS to the Help page
    
    * Added GIFs
    
    * Changed GIFs
    
    * Responsive
    
    * Added GTours for the GuidedTour
    
    * User Tour
    
    First implementation tests for the user tour and adjustments at the help site
    
    * Added Texts for Page Description
    
    * Guided Tour Adjustments
    
    * added anchorlink support
    
    * HelpPage JSON
    
    1. Added Table of contents
    2. Created JSON File
    3. Reading from JSON File
    
    * Filled HelpPage.json
    
    * Using JSON for HelpPage
    
    * Added GIFs for HelpPage
    
    * Adjusted Links in JSON
    
    * Edited HelpPage Texts in JSON File
    
    * Changed JSON File
    
    * User Tour integrated
    
    Added the user tour to all necessary components.
    
    * Fixes
    
    * Tour Fixes
    
    * Style changes
    
    * First Login Tour Start
    
    * Style fix
    
    * Help Page changes
    
    * Added direct Links
    
    * activated user tutorial flag in applicationuser, and added reset in user profile
    
    * Added Welcome Popup
    
    Added a pop up before the first step of the tour (zeroStep)
    
    * Added comments
    
    * Fix close guided Tour
    
    * added new help video, and deprecated german language
    
    ---------
    
    Co-authored-by: Alexander Zender <alexander.zender@h-da.de>

--- frontend/src/Shared/BlazorBoilerplate.Shared/Services/ApiClient.cs
+++ frontend/src/Shared/BlazorBoilerplate.Shared/Services/ApiClient.cs
@@ -19,6 +19,10 @@ namespace BlazorBoilerplate.Shared.Services
     {
         public ApiClient(HttpClient httpClient, ILogger<ApiClient> logger) : base(httpClient, logger)
         { }
+        public async Task<List<HelpPageDto>> GetHelpPageJson()
+        {
+            return await httpClient.GetJsonAsync<List<HelpPageDto>>("/HelpPage.json");
+        }
         public async Task<UserProfile> GetUserProfile()
         {
             return (await entityManager.ExecuteQuery(new EntityQuery<UserProfile>().From("UserProfile"), CancellationToken.None)).SingleOrDefault();
