fe56f7f41ad842537011bbbea2372bee77c3ae4c
jamalissa <57068104+jamalissa@users.noreply.github.com>
2024-06-20 21:24:06 +0200

    compare test scores
    Refactor and Document Code Enhancements
    
    Changes include:
    1. Renamed the "running" phase to "training" for clarity and consistency.
    2. Added docstrings to newly implemented functions to enhance code readability and maintainability.
    3. Implemented logic for comparing test scores

--- controller/blackboard/strategies/PreTrainingStrategy.py
+++ controller/blackboard/strategies/PreTrainingStrategy.py
@@ -83,7 +83,7 @@ class PreTrainingStrategyController(IAbstractStrategy):
         self.controller.get_adapter_runtime_manager().update_adapter_manager_list(relevant_auto_ml_solutions)
 
 
-        self._log.info(f'do_finish_pre_training: Finished data preparation, advancing to phase "running"..')
+        self._log.info(f'do_finish_pre_training: Finished data preparation, advancing to phase "training"..')
         self.controller.set_phase('training')
 
         return
@@ -178,14 +178,31 @@ class PreTrainingStrategyController(IAbstractStrategy):
             controller.disable_strategy('pre_training.finish_pre_training')
 
     def do_optimum_strategy_callback(self, model_list, _):
+        """_summary_
+
+        Args:
+            model_list (_type_): _description_
+            _ (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         # Funktion zum Verarbeiten der abgeschlossenen Modelle und Aktualisieren des Blackboards
         blackboard=self.controller.get_blackboard()
         for model in model_list:
             if model.get('status') == 'completed':
+                #add_data does not exist in the current version of the Blackboard
                 blackboard.add_data({"completed_model": model})
         return model_list
 
     def do_optimum_strategy(self, state: dict, blackboard: Blackboard, controller: StrategyController):
+        """_summary_
+
+        Args:
+            state (dict): _description_
+            blackboard (Blackboard): _description_
+            controller (StrategyController): _description_
+        """
         multi_fidelity_dataset_percentage = 0.8
         initial_runtime_limit = controller.get_request().configuration.runtime_limit
 
@@ -194,12 +211,20 @@ class PreTrainingStrategyController(IAbstractStrategy):
 
         # Deaktiviere Multi-Fidelity-Strategie
         controller.disable_strategy('training.optimum_strategy')
-
         # Starte die erste Iteration
         self.run_iteration(controller, multi_fidelity_dataset_percentage, initial_runtime_limit)
         return
 
     def run_iteration(self, controller: StrategyController, multi_fidelity_dataset_percentage: float, runtime_limit: int):
+        """_summary_
+
+        Args:
+            controller (StrategyController): _description_
+            multi_fidelity_dataset_percentage (float): _description_
+            runtime_limit (int): _description_
+        """
+        top_model={}
+        model_accuracies = {}
         accuracies = []
         consecutive_no_improvement = 0
 
@@ -221,31 +246,62 @@ class PreTrainingStrategyController(IAbstractStrategy):
             # TO-DO
             # user_id von dem training
             user_id = request.user_id
-            print("user_id: ", user_id)
             training_id = controller.get_training_id()
-            print("training_id: ", user_id)
             dataset_id = request.dataset_id
-            print("dataset_id: ", dataset_id)
             model_list = controller.get_data_storage().get_models(user_id,training_id,dataset_id)
-            print("model_list: ", model_list)
+            model_count = len(model_list)
             # Wie kann ich die model_list am besten holen hier ?
-            completed_models = [model for model in model_list if model.get('status') == 'completed']
-            if not completed_models:
-                break
-
-            # Berechne die Genauigkeit
-            accuracy = max(self.get_score(model) for model in completed_models)
+            completed_models = []
+            while model_count!=len(completed_models):
+                model_list = controller.get_data_storage().get_models(user_id,training_id,dataset_id)  
+                #model.get('status') == ('completed' or "failed") it might not work
+                completed_models += [model.get('auto_ml_solution') for model in model_list if model.get('status') in ('completed', 'failed') and model.get('auto_ml_solution') not in completed_models]
+                #time.sleep(5)
+                
+            model_list = controller.get_data_storage().get_models(user_id,training_id,dataset_id)  
+            accuracy = [self.get_score(model) for model in model_list]
+            print("accuracy: ", accuracy)
             accuracies.append(accuracy)
+            #creating key pair value to know which model has which accuracy
+            if completed_models not in model_accuracies:
+                model_accuracies[completed_models] = []
 
+            model_accuracies[completed_models].append(accuracy)
+           # model_accuracies[completed_models] = accuracy
             # Überprüfe, ob eine Verbesserung vorliegt
             epsilon=0.005
-            if accuracies[1] > accuracies[0]+epsilon:
-                consecutive_no_improvement += 1
-            else:
-                consecutive_no_improvement = 0
+            length = len(accuracies)
+            if len(accuracies) >= 2: 
+                # Loop through each pair of consecutive accuracy arrays starting from the second one
+                for i in range(1, len(accuracies)):
+                      previous_accuracies = accuracies[i - 1]
+                      current_accuracies = accuracies[i]
+                      all_smaller_or_equal = True  # Assume they match the condition until proven otherwise
+                    # Check corresponding elements in the two lists
+                    
+                    # Check if each accuracy in the current list is smaller or equal to the previous list's accuracies
+                      for j in range(len(current_accuracies)):
+                            if current_accuracies[j] >= previous_accuracies[j] + epsilon:
+                                all_smaller_or_equal = False
+                                consecutive_no_improvement = 0
+                                break  # Stop checking as soon as one mismatch is found
+
+                        # Output the result of the comparison for each pair of mini-arrays
+                            if all_smaller_or_equal:
+                                consecutive_no_improvement += 1
+                                print(f"Accuracies at index {i} are all smaller or equal to those at index {i-1} + 0.005")
+                            else:
+                                print(f"Accuracies at index {i} are NOT all smaller or equal to those at index {i-1} + 0.005")
+                else:
+                    print("Not enough data to compare accuracies.")
+                print("accuracies: ", accuracies)            
+            
 
             # Laufzeit verdoppeln für die nächste Iteration
             runtime_limit *= 2
+            
+            if consecutive_no_improvement == 2:
+                top_model= max(model_accuracies, key=model_accuracies.get)
 
         self._log.info('Optimum strategy completed.')
         controller.set_phase('completed')
