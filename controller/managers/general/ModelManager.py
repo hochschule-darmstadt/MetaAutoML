076e71da9677d3088682cd31aeed485206c122ae
Keszwel <97045031+Keszwel@users.noreply.github.com>
2024-12-12 22:38:07 +0100

    changed the dashboard status to active or inactive string

--- controller/managers/general/ModelManager.py
+++ controller/managers/general/ModelManager.py
@@ -72,9 +72,9 @@ class ModelManager:
             model_info.status_messages[:] =  model["status_messages"]
             #model_info.explanation = json.dumps(model["explanation"])
             if not model.get("dashboard_path"):  # Returns None if key is missing
-                model_info.dashboard_status = " "
+                model_info.dashboard_status = "inactive"
             else:
-                model_info.dashboard_status = model["dashboard_path"]
+                model_info.dashboard_status = "active"
             if not "carbon_footprint" in model:
                         model["carbon_footprint"] = {"emissions": 0}
             model_info.emission = model["carbon_footprint"].get("emissions", 0)
