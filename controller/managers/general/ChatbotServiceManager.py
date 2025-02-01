97e5bc3ed10d2868413263f870a9f00ee6869b49
jamalissa <Jamalissaz@hotmail.com>
2025-02-02 00:29:05 +0100

    Service manager docker conform

--- controller/managers/general/ChatbotServiceManager.py
+++ controller/managers/general/ChatbotServiceManager.py
@@ -1,15 +1,21 @@
 import asyncio
 from grpclib.client import Channel
+import os
 from ChatbotBGRPC import ChatRequest, ChatReply, ChatbotStub
 import logging
 
 class ChatbotServiceManager:
-    def __init__(self, host: str = 'localhost', port: int = 50051):
-    #def __init__(self, host: str = 'chatbot-container', port: int = 50051):
-        self.host = host
-        self.port = port
+    def __init__(self, host_env_var: str = 'CHATBOT_SERVICE_HOST', port_env_var: str = 'CHATBOT_SERVICE_PORT'):
+        # Retrieve host and port from environment variables
+        #Problem: it cant get the host host_env_var so host.docker.internal is the default (should be localhost)
+        self.host = os.getenv(host_env_var, 'host.docker.internal')
+        print("host",self.host)
+        self.port = int(os.getenv(port_env_var, 50051))
+        print ("port",self.port)
+        # Default to 50051 if not set
         logging.basicConfig(level=logging.INFO)
         self.__log = logging.getLogger('ChatbotServiceManager')
+        self.__log.info(f"Chatbot service will connect to {self.host}:{self.port}")
 
     async def send_message(self, message: str, history: str) -> str:
         """
