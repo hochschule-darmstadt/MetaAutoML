8a8dd2bf7e1342a153001f134e6ef20d1c06314f
jamalissa <Jamalissaz@hotmail.com>
2025-02-02 02:02:25 +0100

    enabling debug and prints to observe results

--- chatbot/rag_pipeline.py
+++ chatbot/rag_pipeline.py
@@ -37,7 +37,7 @@ embeddings = SentenceTransformersEmbeddings(model_name="all-MiniLM-L6-v2")
 retriever = Chroma(persist_directory=database_directory, embedding_function=embeddings,collection_metadata={"hnsw:space": "l2"})
 
 # Initialize LLM
-llm = OllamaLLM(model="llama2:7b-chat")
+llm = OllamaLLM(model="mistral:7b-instruct")
 #llama2:7b-chat
 #gemma2:2b
 #mistral:7b-instruct
